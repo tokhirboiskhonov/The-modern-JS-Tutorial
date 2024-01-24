76bcdae80aa7102577c04a9e68e267e217f059d0
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-24 15:38:37 +0500

    1. I was learning how to write JS code into an HTML file and into itself (JS file).

---
+++ JS-Fundamentals/hello-world.js
@@ -0,0 +1,22 @@
+// This part of the tutorial is about core JavaScript, the language itself.
+alert("Hello world");
+
+// The “script” tag
+// JavaScript programs can be inserted almost anywhere into an HTML document using the <script> tag.
+
+// <!DOCTYPE HTML>
+// <html>
+
+// <body>
+
+//   <p>Before the script...</p>
+
+//   <script>
+//     alert( 'Hello, world!' );
+//   </script>
+
+//   <p>...After the script.</p>
+
+// </body>
+
+// </html>

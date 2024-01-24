b9ebd55e12b73d9fb4b6daf0ec5559911aed0132
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-24 20:42:39 +0500

    feat: add some usefull information about code-structure topic.

---
+++ JS-Fundamentals/code-structure.js
@@ -0,0 +1,13 @@
+// Code Structure
+
+// The first thing you will study is the building blocks of code.
+
+// Semicolons
+// A semicolon may be omitted in most cases when a line break exists.
+
+// This would also work:
+
+// alert("Hello")
+// alert("World")
+
+// In most cases, a newline implies a semicolon. But “in most cases” does not mean “always”!

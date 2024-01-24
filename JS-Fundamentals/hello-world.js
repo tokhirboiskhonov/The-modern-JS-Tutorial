d8a79048cdb2c8805c354c19c7094f17d902f67b
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-24 19:03:55 +0500

    feat: removed some unimportant code.

--- JS-Fundamentals/hello-world.js
+++ JS-Fundamentals/hello-world.js
@@ -4,19 +4,6 @@ alert("Hello world");
 // The “script” tag
 // JavaScript programs can be inserted almost anywhere into an HTML document using the <script> tag.
 
-// <!DOCTYPE HTML>
-// <html>
-
-// <body>
-
-//   <p>Before the script...</p>
-
 //   <script>
 //     alert( 'Hello, world!' );
 //   </script>
-
-//   <p>...After the script.</p>
-
-// </body>
-
-// </html>

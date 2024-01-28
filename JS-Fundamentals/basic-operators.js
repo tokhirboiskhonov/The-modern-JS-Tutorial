9ee91177866c22ce568160d3a7ec522b5cd03d0d
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-28 14:00:32 +0500

    feat: add Modify-in-place into Basic-operators topic.

--- JS-Fundamentals/basic-operators.js
+++ JS-Fundamentals/basic-operators.js
@@ -180,3 +180,33 @@ alert(numX);
 // a = c;
 
 // That’s easier to read, especially when eye-scanning the code fast.
+
+//* Modify-in-place
+
+// We often need to apply an operator to a variable and store the new result in that same variable.
+
+// let n = 2;
+
+// n = n + 5;
+
+// n = n * 2;
+
+// console.log(n);
+
+//! This notation can be shortened using the operators += and *=:
+
+// let n = 2;
+// n += 5; // now n = 7 (same as n = n + 5)
+// n *= 2; // now n = 14 (same as n = n * 2)
+
+// console.log(n); // 14
+
+//! Short “modify-and-assign” operators exist for all arithmetical and bitwise operators: /=, -=, etc.
+
+// Such operators have the same precedence as a normal assignment, so they run after most other calculations:
+
+let n = 2;
+
+n *= 3 + 5; // right part evaluated first, same as n *= 8
+
+alert(n); // 16

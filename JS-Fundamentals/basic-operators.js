f63f66d2859207cdd8a9792975b21f1c06769053
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-28 13:59:15 +0500

    feat: add Assignment topic into Basic-operators unit.

--- JS-Fundamentals/basic-operators.js
+++ JS-Fundamentals/basic-operators.js
@@ -135,3 +135,27 @@ console.log(+apples + +oranges); // 5
 //! Operator precedence
 
 // Go to HTML file
+
+//* Assignment
+
+// Let's note that an assignment = is also an operator.It is listed in the precedence table with the very low priority of 2. That’s why, when we assign a variable, like x = 2 * 2 + 1, the calculations are done first and then the = is evaluated, storing the result in x.
+
+let numX = 2 * 2 + 1;
+alert(numX);
+
+//? Assignment = returns value
+
+// The fact of = being an operator, not a “magical” language construct has an interesting implication.
+
+// All operators in JavaScript return a value. That’s obvious for + and -, but also true for =.
+
+// The call x = value writes the value into x and then returns it.
+
+// Here’s a demo that uses an assignment as part of a more complex expression:
+
+// let a = 1;
+// let b = 2;
+// let c = 3 - (a = b + 1);
+// console.log(c); // 0
+// console.log(a); // 3
+// console.log(b); // 2

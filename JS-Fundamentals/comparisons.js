e4a2c58d924c5cbbbc3c7dc78c54ff6d8e528fb3
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-28 23:34:34 +0500

    feat: add introduction to Comparisons topic.

---
+++ JS-Fundamentals/comparisons.js
@@ -0,0 +1,9 @@
+// We know many comparison operators from maths.
+
+// In JS they are written like this:
+
+//? 1. Greater/less than: a > b, a < b.
+//? 2. Greater/less than or equals: a >= b, a <= b.
+//? 3. Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+//? 4. Not equals: In maths the notation is ≠, but in JavaScript it’s written as a != b.
+

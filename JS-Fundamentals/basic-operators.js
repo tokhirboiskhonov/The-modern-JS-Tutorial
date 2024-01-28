132929af92a42d4dfc4dc04c3601ae68a110e175
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-28 14:01:43 +0500

    feat: add Bitwise operators into Basic-operators topic.

--- JS-Fundamentals/basic-operators.js
+++ JS-Fundamentals/basic-operators.js
@@ -259,3 +259,21 @@ let num = 1;
 
 console.log(2 * num++); // 2
 
+//* Bitwise operators
+
+// Bitwise operators treat arguments as 32-bit integer numbers and work on the level of their binary representation.
+
+// These operators are not JavaScript-specific. They are supported in most programming languages.
+
+// The list of operators:
+
+//? AND ( & )
+//? OR ( | )
+//? XOR ( ^ )
+//? NOT ( ~ )
+//? LEFT SHIFT ( << )
+//? RIGHT SHIFT ( >> )
+//? ZERO-FILL RIGHT SHIFT ( >>> )
+
+// These operators are used very rarely, when we need to fiddle with numbers on the very lowest (bitwise) level. We won’t need these operators any time soon, as web development has little use of them, but in some special areas, such as cryptography, they are useful. You can read the Bitwise Operators chapter on MDN when a need arises.
+

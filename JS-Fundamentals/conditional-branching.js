36e082f8e1125e23601c44a2ea9eea44f5817232
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-29 22:20:55 +0500

    feat: add the if statement to the topic.

--- JS-Fundamentals/conditional-branching.js
+++ JS-Fundamentals/conditional-branching.js
@@ -2,3 +2,23 @@
 
 // Sometimes, we need to perform different actions based on different conditions.
 
+//* The "if" statement
+
+// The if(...) statement evaluates a condition in parentheses and, if the result is true, executes a block of code.
+
+// For example:
+
+// let year = +prompt(
+//   "In which year was ECMAScript-2015 specification published?",
+//   ""
+// );
+
+// if (year == 2015) alert("You are right!");
+
+// if we want to execute more than one statement, we have to wrap our code block inside curly braces:
+
+// if (year == 2015) {
+//   alert("That's correct!");
+//   alert("You're so smart!");
+// }
+

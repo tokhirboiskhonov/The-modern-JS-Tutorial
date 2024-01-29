db5a21284d1f6518112a613e706d66a8b61882c7
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-29 22:21:18 +0500

    feat: add Boolean conversion to the topic.

--- JS-Fundamentals/conditional-branching.js
+++ JS-Fundamentals/conditional-branching.js
@@ -22,3 +22,31 @@
 //   alert("You're so smart!");
 // }
 
+//* Boolean conversion
+
+// The if(...) statement evaluates the expression in its parentheses and converts the result to a boolean.
+
+//? A number 0, an empty string "", null, undefined, and NaN all become false. Because of that they are called “falsy” values.
+
+//? Other values become true, so they are called “truthy”.
+
+// So, the code under this condition would never execute:
+
+// if(0){ // 0 is falsy
+// 	...
+// }
+
+// ...and inside this condition - it always will:
+
+// if(1){ // 1 is truthy
+// 	...
+// }
+
+// We can also pass a pre-evaluated boolean value to if, like this:
+
+// let cond = year == 2015; // equality evaluates to true or false
+
+// if(cond){
+// 	...
+// }
+

6baaf733c9ceeefa7a8f84d212f26bcd36eb1ee7
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-29 22:23:15 +0500

    feat: add multiple "?".

--- JS-Fundamentals/conditional-branching.js
+++ JS-Fundamentals/conditional-branching.js
@@ -110,3 +110,30 @@
 
 // let accessAllowed = age > 18 ? true : false;
 
+//* Multiple ‘?’
+
+// let age = +prompt("age?", 18);
+
+// let message =
+//   age < 3
+//     ? "Hi baby!"
+//     : age < 18
+//     ? "Hello"
+//     : age < 100
+//     ? "Greatings"
+//     : "What an unusual age!";
+
+// alert(message);
+
+// Here’s how this looks using if..else:
+
+// if (age < 3) {
+//   message = "Hi, baby!";
+// } else if (age < 18) {
+//   message = "Hello!";
+// } else if (age < 100) {
+//   message = "Greetings!";
+// } else {
+//   message = "What an unusual age!";
+// }
+

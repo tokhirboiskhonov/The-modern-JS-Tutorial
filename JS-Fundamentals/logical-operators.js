6a5a2cd485453ce87affb77565309aa255717316
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-30 23:52:27 +0500

    feat: add AND "&&" finds the first falsy value to logical-operator.

--- JS-Fundamentals/logical-operators.js
+++ JS-Fundamentals/logical-operators.js
@@ -124,3 +124,63 @@
 //   alert("won't work, because the result is falsy");
 // }
 
+//* AND “&&” finds the first falsy value
+
+// Given multiple AND'ed values:
+
+//? result = value1 && value2 && value3;
+
+//! The AND && operator does the following:
+
+//? 1. Evaluates operands from left to right.
+//? 2. For each operand, converts it to a boolean. If the result is false, stops and returns the original value of that operand.
+//? 3. If all operands have been evaluated (i.e. all were truthy), returns the last operand.
+
+// In other words, AND returns the first falsy value or the last value if none were found.
+
+// The rules above are similar to OR. The difference is that AND returns the first falsy value while OR returns the first truthy one.
+
+// if the first operand is truthy,
+// AND returns the second operand:
+console.log(1 && 0); // 0
+console.log(1 && 5); // 5
+
+// if the first operand is falsy,
+// AND returns it. The second operand is ignored
+console.log(null && 5); // null
+console.log(0 && "no matter what"); // 0
+
+//? We can also pass several values in a row. See how the first falsy one is returned:
+
+console.log(1 && 2 && null && 3); // null
+
+//? When all values are truthy, the last value is returned:
+
+console.log(1 && 2 && 3); // 3, the last one
+
+//! Precedence of AND && is higher than OR ||
+
+// The precedence of AND && operator is higher than OR ||.
+
+// So the code a && b || c && d is essentially the same as if the && expressions were in parentheses: (a && b) || (c && d).
+
+//! Don’t replace if with || or &&
+
+// Sometimes, people use the AND && operator as a "shorter way to write if".
+
+// For instance:
+
+let x = 1;
+
+x > 0 && console.log("Greater than zero!");
+
+// The action in the right part of && would execute only if the evaluation reaches it. That is, only if (x > 0) is true.
+
+// So we basically have an analogue for:
+
+let x1 = 1;
+
+if (x1 > 0) console.log("Greater than zero!");
+
+// Although, the variant with && appears shorter, if is more obvious and tends to be a little bit more readable. So we recommend using every construct for its purpose: use if if we want if and use && if we want AND.
+

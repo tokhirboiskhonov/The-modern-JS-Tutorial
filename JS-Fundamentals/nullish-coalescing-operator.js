e448bca86aaeebc1d04edceefd6c79aef4a6145a
tokhirboiskhonov <tokhir060@gmail.com>
2024-01-31 20:22:25 +0500

    feat: add comparison with || to nullish topic.

--- JS-Fundamentals/nullish-coalescing-operator.js
+++ JS-Fundamentals/nullish-coalescing-operator.js
@@ -50,3 +50,40 @@ console.log(user ?? "Anonymous"); // John (user is not null/undefined)
 // shows the first defined value:
 // alert(firstName ?? lastName ?? nickName ?? "Anonymous"); // Supercoder
 
+//* Comparison with ||
+
+// The OR || operator can be used in the same way as ?? .
+
+// For example, in the code above we could replace ?? with || and still get the same result:
+
+let firstName = null;
+let lastName = null;
+let nickName = "SuperCoder";
+
+console.log(firstName || lastName || nickName || "Anonymous");
+
+// Historically, the OR || operator was there first. It’s been there since the beginning of JavaScript, so developers were using it for such purposes for a long time.
+
+// On the other hand, the nullish coalescing operator ?? was added to JavaScript only recently, and the reason for that was that people weren’t quite happy with ||.
+
+//! The important difference between them is that:
+
+//? 1. || returns the first truthy value.
+//? 2. ?? returns the first defined value.
+
+// In other words, || doesn’t distinguish between false, 0, an empty string "" and null/undefined. They are all the same – falsy values. If any of these is the first argument of ||, then we’ll get the second argument as the result.
+
+// In practice though, we may want to use default value only when the variable is null/undefined. That is, when the value is really unknown/not set.
+
+// let height = 0;
+
+// console.log(height || 100);
+// console.log(height ?? 100);
+
+// 1. The height || 100 checks height for being a falsy value, and it’s 0, falsy indeed.
+// 1.1 so the result of || is the second argument, 100.
+// 2. The height ?? 100 checks height for being null/undefined, and it’s not,
+// 2.2 so the result is height “as is”, that is 0.
+
+// In practice, the zero height is often a valid value, that shouldn’t be replaced with the default. So ?? does just the right thing.
+

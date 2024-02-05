62d4cbdc8a8656e7ad8ac7340ab297fad3583892
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-05 17:38:46 +0500

    feat: add introduction to loop topic.

---
+++ JS-Fundamentals/loops-while-for.js
@@ -0,0 +1,6 @@
+//! Loops: while and for
+
+// We often need to repeat actions.
+// For example, outputting goods from a list one after another or just running the same code for each number from 1 to 10.
+// Loops are a way to repeat the same code multiple times.
+

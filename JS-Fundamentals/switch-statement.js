9a8df43fa163f48d68fd53f78054242bce607803
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-05 19:17:54 +0500

    feat: add into to switch-statement topic.

---
+++ JS-Fundamentals/switch-statement.js
@@ -0,0 +1,8 @@
+// !  The "switch" statement
+
+// A switch statement can replace multiple if checks.
+
+// It gives a more
+
+// It gives a more descriptive way to compare a value with multiple variants.
+

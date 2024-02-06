148db2a586a3e4486ade7676ac4aaa016ba7e177
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-06 16:53:31 +0500

    feat: add naming a function

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -271,3 +271,26 @@ console.log(result);
 
 // alert(doNothing() === undefined); // true
 
+//* Naming a function
+
+// Functions are actions. So their name is usually a verb. It should be brief, as accurate as possible and describe what the function does, so that someone reading the code gets an indication of what the function does.
+
+// It is a widespread practice to start a function with a verbal prefix which vaguely describes the action. There must be an agreement within the team on the meaning of the prefixes.
+
+// For instance, functions that start with "show" usually show something.
+
+// Function starting with…
+
+//? "get…" – return a value,
+//? "calc…" – calculate something,
+//? "create…" – create something,
+//? "check…" – check something and return a boolean, etc.
+
+// Examples of such names:
+
+//? showMessage(..)     // shows a message
+//? getAge(..)          // returns the age (gets it somehow)
+//? calcSum(..)         // calculates a sum and returns the result
+//? createForm(..)      // creates a form (and usually returns it)
+//? checkPermission(..) // checks a permission, returns true/false
+

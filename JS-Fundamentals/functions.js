153b776ebafff7e8d6a8f627bbc5959323e143de
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-06 16:08:57 +0500

    feat: add Global variables to the topic.

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -101,3 +101,11 @@ showMessage();
 
 console.log(userName);
 
+//* Global variables
+
+// Variables declared outside of any function, such as the outer userName in the code above, are called global.
+
+// Global variables are visible from any function (unless shadowed by locals).
+
+// It’s a good practice to minimize the use of global variables. Modern code has few or no globals. Most variables reside in their functions. Sometimes though, they can be useful to store project-level data.
+

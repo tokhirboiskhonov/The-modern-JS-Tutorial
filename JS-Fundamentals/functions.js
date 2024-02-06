c8bcc30e310a095a648611612a14b10b4d4d5361
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-06 16:53:46 +0500

    feat: add summary

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -294,3 +294,27 @@ console.log(result);
 //? createForm(..)      // creates a form (and usually returns it)
 //? checkPermission(..) // checks a permission, returns true/false
 
+//* Summary
+
+// A function declaration looks like this:
+
+// function name(parameters, delimited, by, comma) {
+/* code */
+//  }
+
+//? 1. Values passed to a function as parameters are copied to its local variables.
+//? 2. A function may access outer variables. But it works only from inside out. The code outside of the function doesn’t see its local variables.
+//? 3. A function can return a value. If it doesn’t, then its result is undefined.
+
+// To make the code clean and easy to understand, it’s recommended to use mainly local variables and parameters in the function, not outer variables.
+
+// It is always easier to understand a function which gets parameters, works with them and returns a result than a function which gets no parameters, but modifies outer variables as a side effect.
+
+//! Function naming:
+
+//? 1. A name should clearly describe what the function does. When we see a function call in the code, a good name instantly gives us an understanding what it does and returns.
+//? 2. A function is an action, so function names are usually verbal.
+//? 3. There exist many well-known function prefixes like create…, show…, get…, check… and so on. Use them to hint what a function does.
+
+// Functions are the main building blocks of scripts. Now we’ve covered the basics, so we actually can start creating and using them. But that’s only the beginning of the path. We are going to return to them many times, going more deeply into their advanced features.
+

58e7e232693876611289f75b6e8396a84b0f5045
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-06 16:07:00 +0500

    feat: add Function Declaration to into Functions topic.

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -7,3 +7,35 @@
 // Functions are the main “building blocks” of the program. They allow the code to be called many times without repetition.
 
 // We’ve already seen examples of built-in functions, like alert(message), prompt(message, default) and confirm(question). But we can create functions of our own as well.
+
+//* Function Declaration
+
+// To create a function we can use a function declaration
+
+// It looks like this:
+
+// function showMessage() {
+//   console.log("Hello everyone!");
+// }
+
+// function name(parameter1, parameter2, ...parameterN) {
+//body
+// }
+
+// Our new function can be called by its name: showMessage().
+
+// For instance:
+
+// function showMessage() {
+//   console.log("Hello everyone!");
+// }
+
+// showMessage();
+// showMessage();
+
+// The call showMessage() executes the code of the function. Here we will see the message two times.
+
+// This example clearly demonstrates one of the main purposes of functions: to avoid code duplication.
+
+// If we ever need to change the message or the way it is shown, it’s enough to modify the code in one place: the function which outputs it.
+

9f21ed75a6a3084ac687cde7e316cc573664915f
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-07 19:15:54 +0500

    feat: introduce funtion is that a value.

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -393,3 +393,35 @@ console.log(pow(10, 2)); // 100
 // };
 
 // sayHi();
+
+//* Function is a value
+
+// Let’s reiterate: no matter how the function is created, a function is a value. Both examples above store a function in the sayHi variable.
+
+// We can even print out that value using alert:
+
+// function sayHi() {
+//   alert("Hello");
+// }
+
+// alert(sayHi); // shows the function code
+
+// Please note that the last line does not run the function, because there are no parentheses after sayHi. There are programming languages where any mention of a function name causes its execution, but JavaScript is not like that.
+
+// In JavaScript, a function is a value, so we can deal with it as a value. The code above shows its string representation, which is the source code.
+
+// Surely, a function is a special value, in the sense that we can call it like sayHi().
+
+// But it’s still a value. So we can work with it like with other kinds of values.
+
+// We can copy a function to another variable:
+
+// function sayHi() {
+//   console.log("Hello!");
+// }
+
+// let func = sayHi;
+
+// func();
+// sayHi();
+

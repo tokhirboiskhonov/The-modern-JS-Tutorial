f2336970a52e8b320b806e828194e288f7fb684d
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-07 20:06:42 +0500

    feat: add summary for Arrow-func-basics.

--- JS-Fundamentals/functions.js
+++ JS-Fundamentals/functions.js
@@ -597,3 +597,10 @@ let calcSum = (a, b) => {
 
 console.log(calcSum(3, 2));
 
+//* Summary for Arrow functions-basics.
+
+// Arrow functions are handy for simple actions, especially for one-liners. They come in two flavors:
+
+//? 1. Without curly braces: (...args) => expression – the right side is an expression: the function evaluates it and returns the result. Parentheses can be omitted, if there’s only a single argument, e.g. n => n*2.
+//? 2. With curly braces: (...args) => { body } – brackets allow us to write multiple statements inside the function, but we need an explicit return to return something.
+

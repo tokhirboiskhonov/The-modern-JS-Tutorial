f30e112b294ae55d469523b9b6def3432d1d1121
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-09 01:24:11 +0500

    feat: add the "for ... in" loop.

--- JS-Fundamentals/objects-basic.js
+++ JS-Fundamentals/objects-basic.js
@@ -228,3 +228,30 @@ let user4 = { age: 30 };
 let key2 = "age";
 alert(key in user4); // true, property "age" exists
 
+//* The "for..in" loop
+
+// To walk over all keys of an object, there exists a special form of the loop: for..in. This is a completely different thing from the for(;;) construct that we studied before.
+
+// The syntax:
+
+// for (key in object) {
+// executes the body for each key among object properties
+// }
+
+let userA = {
+  name: "John",
+  age: 30,
+  isAdmin: true,
+};
+
+for (let key in userA) {
+  // keys
+  alert(key); // name, age, isAdmin
+  // values for the keys
+  alert(userA[key]); // John, 30, true
+}
+
+// Note that all “for” constructs allow us to declare the looping variable inside the loop, like let key here.
+
+// Also, we could use another variable name here instead of key. For instance, "for (let prop in obj)" is also widely used.
+

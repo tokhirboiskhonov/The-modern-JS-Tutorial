a93bf17d0364b3c1fb03e57ac193003cf9f96988
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-09 01:25:15 +0500

    feat: add summary for object-basics.

--- JS-Fundamentals/objects-basic.js
+++ JS-Fundamentals/objects-basic.js
@@ -283,3 +283,32 @@ for (let code in codes) {
 // then Switzerland (41) and so on.
 // The phone codes go in the ascending sorted order, because they are integers. So we see 1, 41, 44, 49.
 
+//* Summary
+
+// Objects are associative arrays with several special features.
+
+// They store properties (key-value pairs), where:
+
+// Property keys must be strings or symbols (usually strings).
+// Values can be of any type.
+// To access a property, we can use:
+
+// The dot notation: obj.property.
+// Square brackets notation obj["property"]. Square brackets allow taking the key from a variable, like obj[varWithKey].
+// Additional operators:
+
+// To delete a property: delete obj.prop.
+// To check if a property with the given key exists: "key" in obj.
+// To iterate over an object: for (let key in obj) loop.
+// What we’ve studied in this chapter is called a “plain object”, or just Object.
+
+// There are many other kinds of objects in JavaScript:
+
+// Array to store ordered data collections,
+// Date to store the information about the date and time,
+// Error to store the information about an error.
+
+// …And so on.
+// They have their special features that we’ll study later. Sometimes people say something like “Array type” or “Date type”, but formally they are not types of their own, but belong to a single “object” data type. And they extend it in various ways.
+
+// Objects in JavaScript are very powerful. Here we’ve just scratched the surface of a topic that is really huge. We’ll be closely working with objects and learning more about them in further parts of the tutorial.

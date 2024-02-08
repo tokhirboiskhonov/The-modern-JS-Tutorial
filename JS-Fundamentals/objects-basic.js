e2f10c6608dc58d05d8a92fd9e607e132e1541bd
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-09 01:18:54 +0500

    feat: add intro for Objects-basic topic.

---
+++ JS-Fundamentals/objects-basic.js
@@ -0,0 +1,12 @@
+//! Objects
+
+// As we know from the chapter Data types, there are eight data types in JavaScript. Seven of them are called “primitive”, because their values contain only a single thing (be it a string or a number or whatever).
+
+// In contrast, objects are used to store keyed collections of various data and more complex entities. In JavaScript, objects penetrate almost every aspect of the language. So we must understand them first before going in-depth anywhere else.
+
+// An object can be created with figure brackets {…} with an optional list of properties. A property is a “key: value” pair, where key is a string (also called a “property name”), and value can be anything.
+
+// let user = new Object(); // "object constructor" syntax
+
+// let user = {}; // "object literal" syntax
+

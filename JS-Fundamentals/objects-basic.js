833a1dd77825f6b1b9b0f0351fe1c5990ac61224
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-09 01:20:52 +0500

    feat: add computed properties

--- JS-Fundamentals/objects-basic.js
+++ JS-Fundamentals/objects-basic.js
@@ -111,3 +111,37 @@ let user = {
 let key = "name";
 alert(user.key); // undefined
 
+//* Computed properties
+
+// We can use square brackets in an object literal, when creating an object. That’s called computed properties.
+//
+// let fruit = prompt("Which fruit to buy?", "apple");
+
+// let bag = {
+//   [fruit]: 5, // the name of the property is taken from the variable fruit
+// };
+
+// console.log(bag.apple); // 5 if fruit="apple"
+
+// The meaning of a computed property is simple: [fruit] means that the property name should be taken from fruit.
+
+// So, if a visitor enters "apple", bag will become {apple: 5}.
+
+// Essentially, that works the same as:
+
+let fruit = prompt("Which fruit to buy?", "apple");
+let bag = {};
+
+// take property name from the fruit variable
+bag[fruit] = 5;
+
+// We can use more complex expressions inside square brackets:
+
+// let fruit = 'apple';
+// let bag = {
+//   [fruit + 'Computers']: 5 // bag.appleComputers = 5
+// }
+
+// Square brackets are much more powerful than dot notation. They allow any property names and variables. But they are also more cumbersome to write.
+// So most of the time, when property names are known and simple, the dot is used. And if we need something more complex, then we switch to square brackets.
+

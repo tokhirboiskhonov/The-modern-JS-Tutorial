1c7a28d325412301b1b16aefeac3a55faaf3c491
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-09 01:24:55 +0500

    feat: add ordered like an object.

--- JS-Fundamentals/objects-basic.js
+++ JS-Fundamentals/objects-basic.js
@@ -255,3 +255,31 @@ for (let key in userA) {
 
 // Also, we could use another variable name here instead of key. For instance, "for (let prop in obj)" is also widely used.
 
+//* Ordered like an object
+
+// Are objects ordered? In other words, if we loop over an object, do we get all properties in the same order they were added? Can we rely on this?
+
+// The short answer is: “ordered in a special fashion”: integer properties are sorted, others appear in creation order. The details follow.
+
+// As an example, let’s consider an object with the phone codes:
+
+let codes = {
+  49: "Germany",
+  41: "Switzerland",
+  44: "Great Britain",
+  // ..,
+  1: "USA",
+};
+
+for (let code in codes) {
+  alert(code); // 1, 41, 44, 49
+}
+
+//  The object may be used to suggest a list of options to the user. If we’re making a site mainly for a German audience then we probably want 49 to be the first.
+
+// But if we run the code, we see a totally different picture:
+
+// USA (1) goes first
+// then Switzerland (41) and so on.
+// The phone codes go in the ascending sorted order, because they are integers. So we see 1, 41, 44, 49.
+

36bf5c340ffcaa0d7885dd7965eb033908e9bbef
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-10 16:40:02 +0500

    feat: add summary for object-reference-copying

--- JS-Fundamentals/object-references-copying.js
+++ JS-Fundamentals/object-references-copying.js
@@ -232,3 +232,10 @@ structuredClone({
 
 // To handle such complex cases we may need to use a combination of cloning methods, write custom code or, to not reinvent the wheel, take an existing implementation, for instance _.cloneDeep(obj) from the JavaScript library lodash.
 
+//* Summary
+
+// Objects are assigned and copied by reference. In other words, a variable stores not the “object value”, but a “reference” (address in memory) for the value. So copying such a variable or passing it as a function argument copies that reference, not the object itself.
+
+// All operations via copied references (like adding/removing properties) are performed on the same single object.
+
+// To make a “real copy” (a clone) we can use Object.assign for the so-called “shallow copy” (nested objects are copied by reference) or a “deep cloning” function structuredClone or use a custom cloning implementation, such as _.cloneDeep(obj).

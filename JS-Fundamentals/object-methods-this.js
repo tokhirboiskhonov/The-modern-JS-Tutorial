767de91f40b5aad0dcb84bc92fd2bc2549bac354
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-13 13:58:09 +0500

    feat: add methods examples.

--- JS-Fundamentals/object-methods-this.js
+++ JS-Fundamentals/object-methods-this.js
@@ -11,3 +11,39 @@
 
 // Actions are represented in JavaScript by functions in properties.
 
+//* Methods examples
+
+// For a start, let's teach the user to say hello:
+
+// let user = {
+//   name: "John",
+//   age: 24,
+// };
+
+// user.sayHi = function () {
+//   console.log("Hello!");
+// };
+
+// user.sayHi();
+
+// Here we’ve just used a Function Expression to create a function and assign it to the property user.sayHi of the object.
+
+// Then we can call it as user.sayHi(). The user can now speak!
+
+// A function that is a property of an object is called its method.
+
+// So, here we’ve got a method sayHi of the object user.
+
+// Of course, we could use a pre-declared function as a method, like this:
+
+let obj = {};
+
+// first declare
+function sayHi() {
+  console.log("Hello everybody!");
+}
+
+obj.sayHi = sayHi;
+
+obj.sayHi(); // Hello everybody!
+

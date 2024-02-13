25543ea52b55c48638b8f2fb01d6d29b6a8402ec
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-13 14:02:49 +0500

    feat: add intro about Constructor, operator "new" in JS.

---
+++ JS-Fundamentals/constructor-operator-new.js
@@ -0,0 +1,5 @@
+//* Constructor, operator "new"
+
+// The regular {...} syntax allows us to create one object. But often we need to create many similar objects, like multiple users or menu items and so on.
+
+// That can be done using constructor functions and the "new" operator.

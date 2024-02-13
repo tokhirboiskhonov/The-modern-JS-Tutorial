19a19190d702d4637e07ff5a0e8769edcbebe3ce
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-13 13:57:30 +0500

    feat: add introduction to Object methods topic.

---
+++ JS-Fundamentals/object-methods-this.js
@@ -0,0 +1,13 @@
+//* Object methods, "this"
+
+// Objects are usually created to represent entities of the real world, like users, orders and so on:
+
+// let user = {
+//   name: "John",
+//   age: 24,
+// };
+
+// And, in the real world, a user can act: select something from the shopping cart, login, logout etc.
+
+// Actions are represented in JavaScript by functions in properties.
+

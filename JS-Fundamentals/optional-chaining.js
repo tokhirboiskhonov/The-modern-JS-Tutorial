277da41fcb8e91991bdea47bf83c1622392e5d61
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-16 23:35:06 +0500

    feat: add Optional chaining into the topic.

--- JS-Fundamentals/optional-chaining.js
+++ JS-Fundamentals/optional-chaining.js
@@ -20,3 +20,35 @@
 
 // In many practical cases we’d prefer to get undefined instead of an error here (meaning “no street”).
 
+//* Optional chaining
+
+// The optional chaining ?. stops the evaluation if the value before ?. is undefined or null and returns undefined.
+
+// Further in this article, for brevity, we’ll be saying that something “exists” if it’s not null and not undefined.
+
+//! In other words, value?.prop:
+//? 1. works as value.prop, if value exists,
+//? 2. otherwise (when value is undefined/null) it returns undefined.
+
+// Here’s the safe way to access user.address.street using ?.:
+
+let user = {}; // user has no address
+
+console.log(user?.address?.street); // undefined
+
+//! Don’t overuse the optional chaining
+
+// We should use ?. only where it’s ok that something doesn’t exist.
+
+// For example, if according to our code logic user object must exist, but address is optional, then we should write user.address?.street, but not user?.address?.street.
+
+// Then, if user happens to be undefined, we’ll see a programming error about it and fix it. Otherwise, if we overuse ?., coding errors can be silenced where not appropriate, and become more difficult to debug.
+
+//! The variable before ?. must be declared
+
+// If there’s no variable user at all, then user?.anything triggers an error:
+
+// ReferenceError: user is not defined
+// user?.address;
+// The variable must be declared (e.g. let/const/var user or as a function parameter). The optional chaining works only for declared variables.
+

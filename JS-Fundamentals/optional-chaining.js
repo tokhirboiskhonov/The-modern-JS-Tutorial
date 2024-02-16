6e9bd21715bf13ded10671e0f8f8551f39fd55b3
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-16 23:36:59 +0500

    feat: add Summary to into an optional-chaining.

--- JS-Fundamentals/optional-chaining.js
+++ JS-Fundamentals/optional-chaining.js
@@ -117,3 +117,16 @@ let myUser3 = null;
 myUser3?.name = "John"; // Error, doesn't work
 // because it evaluates to: undefined = "John"
 
+//* Summary
+
+//! The optional chaining ?. syntax has three forms:
+
+//? 1. obj?.prop – returns obj.prop if obj exists, otherwise undefined.
+//? 2. obj?.[prop] – returns obj[prop] if obj exists, otherwise undefined.
+//? 3. obj.method?.() – calls obj.method() if obj.method exists, otherwise returns undefined.
+
+// As we can see, all of them are straightforward and simple to use. The ?. checks the left part for null/undefined and allows the evaluation to proceed if it’s not so.
+
+// A chain of ?. allows to safely access nested properties.
+
+// Still, we should apply ?. carefully, only where it’s acceptable, according to our code logic, that the left part doesn’t exist. So that it won’t hide programming errors from us, if they occur.

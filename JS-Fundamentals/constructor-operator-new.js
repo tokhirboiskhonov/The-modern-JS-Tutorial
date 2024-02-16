ea550227338b3db69fd15b7ef90b3501d1bb9a9e
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-16 12:11:49 +0500

    feat: add some tasks for constructor function.

--- JS-Fundamentals/constructor-operator-new.js
+++ JS-Fundamentals/constructor-operator-new.js
@@ -181,3 +181,45 @@ john = {
 
 // JavaScript provides constructor functions for many built-in language objects: like Date for dates, Set for sets and others that we plan to study.
 
+//* Tasks
+
+//! Create new Calculator
+
+function Calculator(a, b) {
+  this.read = function () {
+    this.a = +prompt("a?", 0);
+    this.b = +prompt("b?", 0);
+  };
+
+  this.sum = function () {
+    return this.a + this.b;
+  };
+
+  this.mul = function () {
+    return this.a * this.b;
+  };
+}
+
+let calculator = new Calculator();
+
+calculator.read();
+
+alert("Sum = " + calculator.sum());
+alert("Mul = " + calculator.mul());
+
+//! Create new Accumulator
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    this.value += +prompt("How much to add?", 0);
+  };
+}
+
+let accumulator = new Accumulator(1);
+
+accumulator.read();
+accumulator.read();
+
+alert(accumulator.value);

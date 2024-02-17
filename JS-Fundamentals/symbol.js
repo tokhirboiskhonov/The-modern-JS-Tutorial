c9682949e52ca0ab0ae04b658e1cf7dad2575485
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-17 23:31:16 +0500

    feat: add symmary to into a Symbol topic.

--- JS-Fundamentals/symbol.js
+++ JS-Fundamentals/symbol.js
@@ -218,3 +218,22 @@ alert(localSymbol.description); // name
 
 // Other symbols will also become familiar when we study the corresponding language features.
 
+//* Summary
+
+// Symbol is a primitive type for unique identifiers.
+
+// Symbols are created with Symbol() call with an optional description (name).
+
+// Symbols are always different values, even if they have the same name. If we want same-named symbols to be equal, then we should use the global registry: Symbol.for(key) returns (creates if needed) a global symbol with key as the name. Multiple calls of Symbol.for with the same key return exactly the same symbol.
+
+// Symbols have two main use cases:
+
+//? 1. “Hidden” object properties.
+
+// If we want to add a property into an object that “belongs” to another script or a library, we can create a symbol and use it as a property key. A symbolic property does not appear in for..in, so it won’t be accidentally processed together with other properties. Also it won’t be accessed directly, because another script does not have our symbol. So the property will be protected from accidental use or overwrite.
+
+// So we can “covertly” hide something into objects that we need, but others should not see, using symbolic properties.
+
+//? 2. There are many system symbols used by JavaScript which are accessible as Symbol.*. We can use them to alter some built-in behaviors. For instance, later in the tutorial we’ll use Symbol.iterator for iterables, Symbol.toPrimitive to setup object-to-primitive conversion and so on.
+
+// Technically, symbols are not 100% hidden. There is a built-in method Object.getOwnPropertySymbols(obj) that allows us to get all symbols. Also there is a method named Reflect.ownKeys(obj) that returns all keys of an object including symbolic ones. But most libraries, built-in functions and syntax constructs don’t use these methods.

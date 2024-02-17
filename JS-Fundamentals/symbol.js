3734380a5455c243c95a2c949a6aa18ee1cac6b4
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-17 23:29:28 +0500

    feat: add Global symbols

--- JS-Fundamentals/symbol.js
+++ JS-Fundamentals/symbol.js
@@ -151,3 +151,26 @@ alert(clone[id6]); // 123
 
 // There’s no paradox here. That’s by design. The idea is that when we clone an object or merge objects, we usually want all properties to be copied (including symbols like id).
 
+//* Global symbols
+
+// As we’ve seen, usually all symbols are different, even if they have the same name. But sometimes we want same-named symbols to be same entities. For instance, different parts of our application want to access symbol "id" meaning exactly the same property.
+
+// To achieve that, there exists a global symbol registry. We can create symbols in it and access them later, and it guarantees that repeated accesses by the same name return exactly the same symbol.
+
+// In order to read (create if absent) a symbol from the registry, use Symbol.for(key).
+
+// That call checks the global registry, and if there’s a symbol described as key, then returns it, otherwise creates a new symbol Symbol(key) and stores it in the registry by the given key.
+
+// For instance:
+
+// read from the global registry
+let id = Symbol.for("id"); // if the symbol did not exist, it is created
+
+// read it again (maybe from another part of the code)
+let idAgain = Symbol.for("id");
+
+// the same symbol
+alert(id === idAgain); // true
+
+// Symbols inside the registry are called global symbols. If we want an application-wide symbol, accessible everywhere in the code – that’s what they are for.
+

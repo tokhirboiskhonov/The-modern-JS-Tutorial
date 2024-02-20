7b5aecb0a31f588c0b13ae0a7dd181dc0947c91d
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-20 22:48:50 +0500

    feat: add intro about Numbers

---
+++ Data-types/numbers.js
@@ -0,0 +1,10 @@
+//* Numbers
+
+// In modern JavaScript, there are two types of numbers:
+
+//? 1. Regular numbers in JavaScript are stored in 64-bit format IEEE-754, also known as “double precision floating point numbers”. These are numbers that we’re using most of the time, and we’ll talk about them in this chapter.
+
+//? 2. BigInt numbers represent integers of arbitrary length. They are sometimes needed because a regular integer number can’t safely exceed (253-1) or be less than -(253-1), as we mentioned earlier in the chapter Data types. As bigints are used in few special areas, we devote them a special chapter BigInt.
+
+// So here we’ll talk about regular numbers. Let’s expand our knowledge of them.
+

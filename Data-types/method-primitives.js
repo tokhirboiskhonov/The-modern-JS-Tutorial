4b6c57fe9b545fdd9306a5a6b65382e0eccc765f
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-20 21:09:13 +0500

    feat: add a primitive as an object.

--- Data-types/method-primitives.js
+++ Data-types/method-primitives.js
@@ -32,3 +32,30 @@ john.sayHi();
 
 // Objects are “heavier” than primitives. They require additional resources to support the internal machinery.
 
+//* A primitive as an object
+
+//! Here’s the paradox faced by the creator of JavaScript:
+
+//? *There are many things one would want to do with a primitive, like a string or a number. It would be great to access them using methods.
+//? *Primitives must be as fast and lightweight as possible.
+
+//! The solution looks a little bit awkward, but here it is:
+
+//? * Primitives are still primitive. A single value, as desired.
+//? * The language allows access to methods and properties of strings, numbers, booleans and symbols.
+//? * In order for that to work, a special “object wrapper” that provides the extra functionality is created, and then is destroyed.
+
+// The “object wrappers” are different for each primitive type and are called: String, Number, Boolean, Symbol and BigInt. Thus, they provide different sets of methods.
+
+// For instance, there exists a string method str.toUpperCase() that returns a capitalized str.
+
+// Here’s how it works:
+
+let str = "hello";
+
+console.log(str.toUpperCase()); // HELLO
+
+let n = 1.23456;
+
+console.log(n.toFixed()); // 1
+

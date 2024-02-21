d56515beba081bee8d9139a9d50ba511b496e24e
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-21 21:25:42 +0500

    feat: explained how to call toString method using two dots with numerical number.

--- Data-types/numbers.js
+++ Data-types/numbers.js
@@ -95,3 +95,11 @@ console.log(num.toString(2)); // 11111111
 
 console.log((123456).toString(36)); // 2n9c
 
+//! Two dots to call a method
+
+// Please note that two dots in 123456..toString(36) is not a typo. If we want to call a method directly on a number, like toString in the example above, then we need to place two dots .. after it.
+
+// If we placed a single dot: 123456.toString(36), then there would be an error, because JavaScript syntax implies the decimal part after the first dot. And if we place one more dot, then JavaScript knows that the decimal part is empty and now goes the method.
+
+// Also could write (123456).toString(36).
+

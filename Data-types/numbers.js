384899567c2f7e7c468be94da53f27b6cf461444
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-21 21:24:38 +0500

    feat: add toString(base) method to into a number.

--- Data-types/numbers.js
+++ Data-types/numbers.js
@@ -72,3 +72,26 @@ alert(a == b); // true, the same number 255 at both sides
 
 // There are only 3 numeral systems with such support. For other numeral systems, we should use the function parseInt (which we will see later in this chapter).
 
+//* toString(base)
+
+// The method num.toString(base) returns a string representation of num in the numeral system with the given base.
+
+//For example:
+
+let num = 255;
+
+console.log(num.toString(16)); // ff
+console.log(num.toString(2)); // 11111111
+
+// The base can vary from 2 to 36. By default it’s 10.
+
+// Common use cases for this are:
+
+// 1. base=16 is used for hex colors, character encodings etc, digits can be 0..9 or A..F.
+
+// 2. base=2 is mostly for debugging bitwise operations, digits can be 0 or 1.
+
+// 3. base=36 is the maximum, digits can be 0..9 or A..Z. The whole latin alphabet is used to represent a number. A funny, but useful case for 36 is when we need to turn a long numeric identifier into something shorter, for example to make a short url. Can simply represent it in the numeral system with base 36:
+
+console.log((123456).toString(36)); // 2n9c
+

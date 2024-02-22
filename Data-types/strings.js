b055a65ebe01fe95202a8e21020ba1f73f7dab70
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-22 20:14:32 +0500

    feat: add intro to strings in JS.

---
+++ Data-types/strings.js
@@ -0,0 +1,6 @@
+//* Strings
+
+// In JavaScript, the textual data is stored as strings. There is no separate type for a single character.
+
+// The internal format for strings is always UTF-16, it is not tied to the page encoding.
+

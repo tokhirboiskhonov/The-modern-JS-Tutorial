6de09c3e27d0548caeaccfb719023af2501efe72
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-26 16:44:05 +0500

    feat: add String.fromCodePoint(code)

--- Data-types/strings.js
+++ Data-types/strings.js
@@ -354,3 +354,10 @@ alert("Z".codePointAt(0)); // 90
 alert("z".codePointAt(0)); // 122
 alert("z".codePointAt(0).toString(16)); // 7a (if we need a hexadecimal value)
 
+//! String.fromCodePoint(code)
+
+// Creates a character by its numeric code
+
+alert(String.fromCodePoint(90)); // Z
+alert(String.fromCodePoint(0x5a)); // Z (we can also use a hex value as an argument)
+

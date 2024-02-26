a857f52e4d6ae9c4be9e6a05aa5bf5f5deaefd28
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-26 16:19:05 +0500

    feat: add indexOf to into String.

--- Data-types/strings.js
+++ Data-types/strings.js
@@ -174,3 +174,49 @@ alert("Interface"[0].toLowerCase()); // 'i'
 
 // There are multiple ways to look for a substring within a string.
 
+//! str.indexOf
+
+// The first method is str.indexOf(substr, pos).
+
+// It looks for the substr in str, starting from the given position pos, and returns the position where the match was found or -1 if nothing can be found.
+
+let firstStr = "Widget with id";
+
+alert(firstStr.indexOf("Widget")); // 0, because 'Widget' is found at the beginning
+alert(firstStr.indexOf("widget")); // -1, not found, the search is case-sensitive
+
+alert(firstStr.indexOf("id")); // 1, "id" is found at the position 1 (..idget with id)
+
+// The optional second parameter allows us to start searching from a given position.
+
+// For instance, the first occurrence of "id" is at position 1. To look for the next occurrence, let’s start the search from position 2:
+
+let strIndexOf = "Widget with id";
+
+alert(strIndexOf.indexOf("id", 2)); // 12
+
+// If we’re interested in all occurrences, we can run indexOf in a loop. Every new call is made with the position after the previous match:
+
+let str12 = "As sly as a fox, as strong as an ox";
+
+let target = "as"; // let's look for it
+
+let pos = 0;
+while (true) {
+  let foundPos = str12.indexOf(target, pos);
+  if (foundPos == -1) break;
+
+  alert(`Found at ${foundPos}`);
+  pos = foundPos + 1; // continue the search from the next position
+}
+
+// The same algorithm can be layed out shorter:
+
+let str3 = "As sly as a fox, as strong as an ox";
+let target3 = "as";
+
+let pos3 = -1;
+while ((pos3 = str3.indexOf(target3, pos3 + 1)) != -1) {
+  alert(pos3);
+}
+

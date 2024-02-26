f4639ad26633e5aa1faf16b54efb0e0e9e988490
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-26 16:46:47 +0500

    feat: add last one task to into String topic.

--- Data-types/strings.js
+++ Data-types/strings.js
@@ -450,3 +450,15 @@ console.log(
   truncate("Hello everyone! How are you doing? How is your family?", 20)
 );
 
+//! 4. Extract the money
+
+// We have a cost in the form "$120". That is: the dollar sign goes first, and then the number.
+
+// Create a function extractCurrencyValue(str) that would extract the numeric value from such string and return it.
+
+function extractCurrencyValue(str) {
+  return +str.slice(1);
+}
+
+console.log(extractCurrencyValue("$120")); // 120
+console.log(extractCurrencyValue("$120") == 120); // true

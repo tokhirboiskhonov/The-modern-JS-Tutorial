491520adbd2b808d4e8550384a9284e51bedbad2
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-26 16:43:42 +0500

    feat: add str.codePointAt(pos)

--- Data-types/strings.js
+++ Data-types/strings.js
@@ -346,3 +346,11 @@ alert("Österreich" > "Zealand"); // true
 
 // There are special methods that allow to get the character for the code and back:
 
+//! str.codePointAt(pos)
+// Returns a decimal number representing the code for the character at position pos:
+
+// different case letters have different codes
+alert("Z".codePointAt(0)); // 90
+alert("z".codePointAt(0)); // 122
+alert("z".codePointAt(0).toString(16)); // 7a (if we need a hexadecimal value)
+

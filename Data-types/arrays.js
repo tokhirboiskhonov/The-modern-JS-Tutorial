a6255406f02653f3f5d96d8045a21b098ad2c394
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-27 23:05:09 +0500

    feat: add methods such as pop and push to into an Arrays

--- Data-types/arrays.js
+++ Data-types/arrays.js
@@ -131,3 +131,27 @@ alert(fruits.at(-1)); // Plum
 
 // So new elements are added or taken always from the “end”.
 
+//! Methods that work with the end of the array:
+
+//* pop
+
+let fruits = ["Apple", "Orange", "Pear"];
+
+alert(fruits.pop()); // remove "Pear" and alert it
+
+alert(fruits); // Apple, Orange
+
+// Both fruits.pop() and fruits.at(-1) return the last element of the array, but fruits.pop() also modifies the array by removing it.
+
+//* push
+
+// Append the element to the end of the array:
+
+let fruits = ["Apple", "Orange"];
+
+fruits.push("Pear");
+
+alert(fruits); // Apple, Orange, Pear
+
+// The call fruits.push(...) is equal to fruits[fruits.length] = ....
+

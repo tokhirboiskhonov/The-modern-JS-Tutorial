c4d7d80fb5b304fc197207a39a4df4ecb2bc0271
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-27 23:02:03 +0500

    feat: add introduction to Arrays topic.

---
+++ Data-types/arrays.js
@@ -0,0 +1,10 @@
+//* Arrays
+
+// Objects allow you to store keyed collections of values. That’s fine.
+
+// But quite often we find that we need an ordered collection, where we have a 1st, a 2nd, a 3rd element and so on. For example, we need that to store a list of something: users, goods, HTML elements etc.
+
+// It is not convenient to use an object here, because it provides no methods to manage the order of elements. We can’t insert a new property “between” the existing ones. Objects are just not meant for such use.
+
+// There exists a special data structure named Array, to store ordered collections.
+

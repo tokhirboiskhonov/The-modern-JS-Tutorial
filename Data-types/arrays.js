40dee0aab8045ad3858cce955d72581d9e652b54
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-28 21:26:09 +0500

    feat: add Summary to into an Array.

--- Data-types/arrays.js
+++ Data-types/arrays.js
@@ -350,3 +350,49 @@ alert("" + 1); // "1"
 alert("1" + 1); // "11"
 alert("1,2" + 1); // "1,21"
 
+//* Summary
+
+// Array is a special kind of object, suited to storing and managing ordered data items.
+
+// The declaration:
+
+// square brackets (usual)
+let arr10 = [item1, item2];
+
+// new Array (exceptionally rare)
+let arr11 = new Array(item1, item2);
+
+//! The call to new Array(number) creates an array with the given length, but without elements.
+
+// 1. The length property is the array length or, to be precise, its last numeric index plus one. It is auto-adjusted by array methods.
+
+// 2. If we shorten length manually, the array is truncated.
+
+//! Getting the elements:
+
+// 1. we can get element by its index, like arr[0]
+
+// 2. also we can use at(i) method that allows negative indexes. For negative values of i, it steps back from the end of the array. If i >= 0, it works same as arr[i].
+
+//! We can use an array as a deque with the following operations:
+
+// 1. push(...items) adds items to the end.
+
+// 2. pop() removes the element from the end and returns it.
+
+// 3. shift() removes the element from the beginning and returns it.
+
+// 4. unshift(...items) adds items to the beginning.
+
+//! To loop over the elements of the array:
+
+// 1. for (let i=0; i<arr.length; i++) – works fastest, old-browser-compatible.
+// 2. for (let item of arr) – the modern syntax for items only,
+// 3. for (let i in arr) – never use.
+
+// To compare arrays, don’t use the == operator (as well as >, < and others), as they have no special treatment for arrays. They handle them as any objects, and it’s not what we usually want.
+
+// Instead you can use for..of loop to compare arrays item-by-item.
+
+// We will continue with arrays and study more methods to add, remove, extract elements and sort arrays in the next chapter Array methods.
+

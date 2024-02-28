84d734dab84e0efbd84576ae810da178fdc1c743
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-28 22:39:55 +0500

    feat: explain that how to iterate using with forEach in Arrays.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -150,4 +150,25 @@ let arrayLike1 = {
 
 alert(arr9.concat(arrayLike1)); // 1,2,something,else
 
+//* Iterate: forEach
+
+// The arr.forEach method allows to run a function for every element of the array.
+
+// The syntax:
+
+//? arr.forEach(function(item, index, array) {
+// ... do something with item
+// });
+
+// For instance, this shows each element of the array:
+
+// for each element call alert
+["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
+
+// And this code is more elaborate about their positions in the target array:
+
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  console.log(`${item} is at index ${index} in ${array}`);
+});
+
 // The result of the function (if it returns any) is thrown away and ignored.

d87528ba6c4ec26b0df5b2093bec772fd4766b11
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-28 21:24:32 +0500

    feat: add info about a word about 'length'.

--- Data-types/arrays.js
+++ Data-types/arrays.js
@@ -268,3 +268,30 @@ for (let key in arr5) {
 
 //! Generally, we shouldn’t use for..in for arrays.
 
+//* A word about "length"
+
+// The length property automatically updates when we modify the array. To be precise, it is actually not the count of values in the array, but the greatest numeric index plus one.
+
+// For instance, a single element with a large index gives a big length:
+
+let fruits = [];
+fruits[123] = "Apple";
+
+alert(fruits.length); // 124
+
+// Note that we usually don’t use arrays like that.
+
+// Another interesting thing about the length property is that it’s writable.
+
+// If we increase it manually, nothing interesting happens. But if we decrease it, the array is truncated. The process is irreversible, here’s the example:
+
+let arr6 = [1, 2, 3, 4, 5];
+
+arr6.length = 2; // truncate to 2 elements
+alert(arr6); // [1, 2]
+
+arr6.length = 5; // return length back
+alert(arr6[3]); // undefined: the values do not return
+
+// So, the simplest way to clear the array is: arr.length = 0;
+

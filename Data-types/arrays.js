0107c582a6120607b4bb5fb7cb1096ffd247c918
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-28 21:24:07 +0500

    feat: add Loops to into an Array.

--- Data-types/arrays.js
+++ Data-types/arrays.js
@@ -228,3 +228,43 @@ fruits.age = 25; // create a property with an arbitrary name
 //? 3. Fill the array in the reverse order, like arr[1000], arr[999] and so on.
 
 // Please think of arrays as special structures to work with the ordered data. They provide special methods for that. Arrays are carefully tuned inside JavaScript engines to work with contiguous ordered data, please use them this way. And if you need arbitrary keys, chances are high that you actually require a regular object {}.
+
+//* Loops
+
+// One of the oldest ways to cycle array items is the for loop over indexes:
+
+let arr4 = ["Apple", "Orange", "Pear"];
+
+for (let i = 0; i < arr4.length; i++) {
+  alert(arr4[i]);
+}
+
+// But for arrays there is another form of loop, for..of:
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+// iterates over array elements
+for (let fruit of fruits) {
+  console.log(fruit); // Apple Orange Plum
+}
+
+// The for..of doesn’t give access to the number of the current element, just its value, but in most cases that’s enough. And it’s shorter.
+
+// Technically, because arrays are objects, it is also possible to use for..in:
+
+let arr5 = ["Apple", "Orange", "Pear"];
+
+for (let key in arr5) {
+  alert(arr5[key]); // Apple, Orange, Pear
+}
+
+// But that’s actually a bad idea. There are potential problems with it:
+
+// 1. The loop for..in iterates over all properties, not only the numeric ones.
+
+// There are so-called “array-like” objects in the browser and in other environments, that look like arrays. That is, they have length and indexes properties, but they may also have other non-numeric properties and methods, which we usually don’t need. The for..in loop will list them though. So if we need to work with array-like objects, then these “extra” properties can become a problem.
+
+// 2. The for..in loop is optimized for generic objects, not arrays, and thus is 10-100 times slower. Of course, it’s still very fast. The speedup may only matter in bottlenecks. But still we should be aware of the difference.
+
+//! Generally, we shouldn’t use for..in for arrays.
+

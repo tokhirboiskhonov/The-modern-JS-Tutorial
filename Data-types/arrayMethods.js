4c052ae90c490ff84a854cf61df9de1fd6d51f9d
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-28 22:37:47 +0500

    feat: add slice() methods to into an Arrays.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -77,3 +77,25 @@ arr5.splice(-1, 0, 3, 4);
 
 alert(arr5); // 1,2,3,4,5
 
+//! slice
+
+// The method arr.slice is much simpler than similar-looking arr.splice.
+
+// The syntax is:
+
+//? arr.slice([start], [end]);
+
+// It returns a new array copying to it all items from index start to end (not including end). Both start and end can be negative, in that case position from array end is assumed.
+
+// It’s similar to a string method str.slice, but instead of substrings it makes subarrays.
+
+// For instance:
+
+let arr6 = ["t", "e", "s", "t"];
+
+alert(arr6.slice(1, 3)); // e,s (copy from 1 to 3)
+
+alert(arr6.slice(-2)); // s,t (copy from -2 till the end)
+
+// We can also call it without arguments: arr.slice() creates a copy of arr. That’s often used to obtain a copy for further transformations that should not affect the original array.
+

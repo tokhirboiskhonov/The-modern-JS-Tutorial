100c92a0bbadf8113cea418ba25c776d0c0822d4
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-29 23:42:05 +0500

    feat: add Array.isArray() methods to into an Array.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -405,3 +405,17 @@ console.log(result1); // 15
 
 //? The function passed to reduce uses only 2 arguments, that’s typically enough.
 
+//* Array.isArray()
+
+// Arrays do not form a separate language type. They are based on objects.
+// So typeof does not help to distinguish a plain object from an array:
+
+console.log(typeof {}); // object
+console.log(typeof []); // object (same)
+
+// ...But arrays are used so often that there’s a special method for that: Array.isArray(value). It returns true if the value is an array, and false otherwise.
+
+console.log(Array.isArray({})); // false
+
+console.log(Array.isArray([])); // true
+

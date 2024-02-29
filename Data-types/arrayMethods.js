d6cbb5f6d14e4cb0cbefb8a868cb85634c79cfa6
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-29 23:38:10 +0500

    feat: add filter method to into an Array.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -254,3 +254,29 @@ alert(users1.findIndex((user) => user.name == "John")); // 0
 // Find the index of the last John
 alert(users1.findLastIndex((user) => user.name == "John")); // 3
 
+//! filter
+
+// The find method looks for a single (first) element that makes the function return true.
+
+// If there may be many, we can use arr.filter(fn).
+
+// The syntax is similar to find, but filter returns an array of all matching elements:
+
+let results = arr.filter(function (item, index, array) {
+  // if true item is pushed to results and the iteration continues
+  // returns empty array if nothing found
+});
+
+// For instance:
+
+let users2 = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+];
+
+// returns array of the first two users
+let someUsers = users2.filter((item) => item.id < 3);
+
+alert(someUsers.length); // 2
+

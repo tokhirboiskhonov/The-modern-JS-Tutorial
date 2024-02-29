72b4c176ae75f02ecf24b99f7686791cdc878b04
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-29 23:35:45 +0500

    feat: add find and findIndex/findLastIndex methods to into an Array.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -204,3 +204,53 @@ alert(arr11.includes(NaN)); // true (correct)
 
 // That’s because includes was added to JavaScript much later and uses the more up to date comparison algorithm internally.
 
+//* find and findIndex/findLastIndex
+
+// The syntax is:
+
+// let result = arr.find(function(item, index, array) {
+// if true is returned, item is returned and iteration is stopped
+// for falsy scenario returns undefined
+// });
+
+// The function is called for elements of the array, one after another:
+
+// 1. item is the element.
+// 2. index is its index.
+// 3. array is the array itself.
+
+// If it returns true, the search is stopped, the item is returned. If nothing found, undefined is returned.
+
+// For example, we have an array of users, each with the fields id and name. Let’s find the one with id == 1:
+
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+];
+
+let user = users.find((item) => item.id == 1);
+
+console.log(user.name); // John
+
+// In real life arrays of objects is a common thing, so the find method is very useful.
+
+//? The arr.findIndex method has the same syntax, but returns the index where the element was found instead of the element itself. The value of -1 is returned if nothing is found.
+
+//? The arr.findLastIndex method is like findIndex, but searches from right to left, similar to lastIndexOf.
+
+// Here’s an example:
+
+let users1 = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+  { id: 4, name: "John" },
+];
+
+// Find the index of the first John
+alert(users1.findIndex((user) => user.name == "John")); // 0
+
+// Find the index of the last John
+alert(users1.findLastIndex((user) => user.name == "John")); // 3
+

b923950b006156df8bc1f475cce7d52bff779c4c
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-29 23:39:03 +0500

    feat: add map methods to into an Array.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -284,3 +284,20 @@ alert(someUsers.length); // 2
 
 // Let’s move on to methods that transform and reorder an array.
 
+//! map
+
+// The arr.map method is one of the most useful and often used.
+
+// It calls the function for each element of the array and returns the array of results.
+
+// The syntax is:
+
+let result = arr.map(function (item, index, array) {
+  // returns the new value instead of item
+});
+
+// For instance, here we transform each element into its length:
+
+let lengths = ["Bilbo", "Gandalf", "Nazgul"].map((item) => item.length);
+alert(lengths); // 5,7,6
+

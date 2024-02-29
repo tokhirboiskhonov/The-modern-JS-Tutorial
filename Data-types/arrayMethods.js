a699326cbad2ff26a4516d0fcef5b4ebd01f6988
tokhirboiskhonov <tokhir060@gmail.com>
2024-02-29 23:42:39 +0500

    feat: add thisArg methods in Arrays.

--- Data-types/arrayMethods.js
+++ Data-types/arrayMethods.js
@@ -419,3 +419,41 @@ console.log(Array.isArray({})); // false
 
 console.log(Array.isArray([])); // true
 
+//* Most methods support “thisArg”
+
+// Almost all array methods that call functions – like find, filter, map, with a notable exception of sort, accept an optional additional parameter thisArg.
+
+// That parameter is not explained in the sections above, because it’s rarely used. But for completeness we have to cover it.
+
+// Here’s the full syntax of these methods:
+
+arr.find(func, thisArg);
+arr.filter(func, thisArg);
+arr.map(func, thisArg);
+// ...
+// thisArg is the optional last argument
+
+// The value of thisArg parameter becomes this for func.
+// For example, here we use a method of army object as a filter, and thisArg passes the context:
+
+let army = {
+  minAge: 18,
+  maxAge: 27,
+  canJoin(user) {
+    return user.age >= this.minAge && user.age < this.maxAge;
+  },
+};
+
+let users3 = [{ age: 16 }, { age: 20 }, { age: 23 }, { age: 30 }];
+
+// find users, for who army.canJoin returns true
+let soldiers = users3.filter(army.canJoin, army);
+
+alert(soldiers.length); // 2
+alert(soldiers[0].age); // 20
+alert(soldiers[1].age); // 23
+
+// If in the example above we used users.filter(army.canJoin), then army.canJoin would be called as a standalone function, with this=undefined, thus leading to an instant error.
+
+// A call to users.filter(army.canJoin, army) can be replaced with users.filter(user => army.canJoin(user)), that does the same. The latter is used more often, as it’s a bit easier to understand for most people.
+

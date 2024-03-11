dc844d6446efd6866dfd49307ba7f45065c41591
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-11 11:56:37 +0500

    feat: add intro to into Iterables topic.

---
+++ Data-types/iterables.js
@@ -0,0 +1,8 @@
+//* Iterables
+
+// Iterable objects are a generalization of arrays. That’s a concept that allows us to make any object useable in a for..of loop.
+
+// Of course, Arrays are iterable. But there are many other built-in objects, that are iterable as well. For instance, strings are also iterable.
+
+// If an object isn’t technically an array, but represents a collection (list, set) of something, then for..of is a great syntax to loop over it, so let’s see how to make it work.
+

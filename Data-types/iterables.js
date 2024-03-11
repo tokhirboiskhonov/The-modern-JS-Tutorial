6391acbdc0239d375f2fc40edf46d0323f61f141
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-11 11:59:46 +0500

    feat: iterables and array-likes.

--- Data-types/iterables.js
+++ Data-types/iterables.js
@@ -136,3 +136,31 @@ while (true) {
 
 // That is rarely needed, but gives us more control over the process than for..of. For instance, we can split the iteration process: iterate a bit, then stop, do something else, and then resume later.
 
+//* Iterables and array-likes
+
+// Two official terms look similar, but are very different. Please make sure you understand them well to avoid the confusion.
+
+//? Iterables are objects that implement the Symbol.iterator method, as described above.
+//? Array-likes are objects that have indexes and length, so they look like arrays.
+
+// When we use JavaScript for practical tasks in a browser or any other environment, we may meet objects that are iterables or array-likes, or both.
+
+// For instance, strings are both iterable (for..of works on them) and array-like (they have numeric indexes and length).
+
+// But an iterable may be not array-like. And vice versa an array-like may be not iterable.
+
+// For example, the range in the example above is iterable, but not array-like, because it does not have indexed properties and length.
+
+// And here’s the object that is array-like, but not iterable:
+
+// let arrayLike = {
+//   0: "Hello",
+//   1: "World",
+//   length: 2,
+// };
+
+// Error (no Symbol.iterator)
+// for(let item of arrayLike){}
+
+// Both iterables and array-likes are usually not arrays, they don’t have push, pop etc. That’s rather inconvenient if we have such an object and want to work with it as with an array. E.g. we would like to work with range using array methods. How to achieve that?
+

f1a4ead1b38fdb1770534b9df14970bf5c8933ee
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-13 12:31:32 +0500

    feat: add intro to into a Map

--- Data-types/map-set.js
+++ Data-types/map-set.js
@@ -1,3 +1,8 @@
 //* Map and Set
 
 //! Till now, we’ve learned about the following complex data structures:
+
+//? Objects are used for storing keyed collections.
+
+//? Arrays are used for storing ordered collections.
+

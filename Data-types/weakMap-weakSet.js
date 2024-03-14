5027352c556ce96c3e653e5ea2a2ae8d39976c3a
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-14 16:54:53 +0500

    feat: learn about WeakMap

--- Data-types/weakMap-weakSet.js
+++ Data-types/weakMap-weakSet.js
@@ -50,3 +50,45 @@ console.log(map);
 
 // Let’s see what it means on examples.
 
+//* WeakMap
+
+//? The first difference between Map and WeakMap is that keys must be objects, not primitive values:
+
+let weakMap = new WeakMap();
+
+let obj = {};
+
+weakMap.set(obj, "ok");
+// console.log(weakMap.get(obj)); // output is ok (works fine (object key))
+
+weakMap.set("test", "Wassup?");
+
+console.log(weakMap.get("test")); // TypeError: Invalid value used as weak map key, so it means can't use primitive values with WeakMap
+
+// Now, if we use an object as the key in it, and there are no other references to that object – it will be removed from memory (and from the map) automatically.
+
+let name = { name: "Tokhirkhuja" };
+
+let weakMap1 = new WeakMap();
+
+weakMap1.set(name, "...");
+
+name = null; // overwrite the reference
+
+console.log(weakMap1); // WeakMap { <items unknown> }
+
+// name is removed from memory!
+
+// Compare it with the regular Map example above. Now if john only exists as the key of WeakMap – it will be automatically deleted from the map (and memory).
+
+//! WeakMap does not support iteration and methods keys(), values(), entries(), so there’s no way to get all keys or values from it.
+
+// WeakMap has only the following methods:
+
+//? weakMap.set(key, value)
+//? weakMap.get(key)
+//? weakMap.delete(key)
+//? weakMap.has(key)
+
+// Now, where do we need such a data structure?
+

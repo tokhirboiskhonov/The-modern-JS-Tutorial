dd2b15ad6db9f0def4392022f7488d007817e5bf
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-14 17:02:21 +0500

    feat: add Summary to the topic

--- Data-types/weakMap-weakSet.js
+++ Data-types/weakMap-weakSet.js
@@ -211,3 +211,15 @@ john = null;
 
 // The most notable limitation of WeakMap and WeakSet is the absence of iterations, and the inability to get all current content. That may appear inconvenient, but does not prevent WeakMap/WeakSet from doing their main job – be an “additional” storage of data for objects which are stored/managed at another place.
 
+//* Summary
+
+// WeakMap is Map-like collection that allows only objects as keys and removes them together with associated value once they become inaccessible by other means.
+
+// WeakSet is Set-like collection that stores only objects and removes them once they become inaccessible by other means.
+
+// Their main advantages are that they have weak reference to objects, so they can easily be removed by garbage collector.
+
+// That comes at the cost of not having support for clear, size, keys, values…
+
+// WeakMap and WeakSet are used as “secondary” data structures in addition to the “primary” object storage. Once the object is removed from the primary storage, if it is only found as the key of WeakMap or in a WeakSet, it will be cleaned up automatically.
+

76746213cab311f6b4fc71b4fd8d17d63052e307
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-14 21:30:23 +0500

    feat: add more info about Object.keys, values, entries.

--- Data-types/object-keys-values-entries.js
+++ Data-types/object-keys-values-entries.js
@@ -14,3 +14,45 @@
 
 // Plain objects also support similar methods, but the syntax is a bit different.
 
+//* Object.keys, values, entries
+
+// For plain objects, the following methods are available:
+
+//? Object.keys(obj) – returns an array of keys.
+//? Object.values(obj) – returns an array of values.
+//? Object.entries(obj) – returns an array of [key, value] pairs.
+
+// Please note the distinctions (compared to map for example):
+
+// 					Map			Object
+// Call syntax:	map.keys()	Object.keys(obj), but not obj.keys()
+// Returns:			iterable		“real” Array
+
+// The first difference is that we have to call Object.keys(obj), and not obj.keys().
+
+// Why so? The main reason is flexibility. Remember, objects are a base of all complex structures in JavaScript. So we may have an object of our own like data that implements its own data.values() method. And we still can call Object.values(data) on it.
+
+// The second difference is that Object.* methods return “real” array objects, not just an iterable. That’s mainly for historical reasons.
+
+// For instance:
+
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+
+//? Object.keys(user) = ["name", "age"]
+//? Object.values(user) = ["John", 30]
+//? Object.entries(user) = [ ["name","John"], ["age",30] ]
+
+// Here’s an example of using Object.values to loop over property values:
+
+let user = {
+  name: "John",
+  age: 30,
+};
+
+// loop over values
+for (let value of Object.values(user)) {
+  console.log(value); // John, then 30
+}

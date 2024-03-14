e509156cfdbaa85ab4c0400229a7fde6e30f9715
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-14 21:29:42 +0500

    feat: add intro to an Object-keys-values-entries topic.

---
+++ Data-types/object-keys-values-entries.js
@@ -0,0 +1,16 @@
+//* Object.keys, values, entries
+
+// Let’s step away from the individual  data structures and talk about the iterations over them.
+
+// In the previous chapter we saw methods map.keys(), map.values(), map.entries().
+
+// These methods are generic, there is a common agreement to use them for data structures. If we ever create a data structure of our own, we should implement them too.
+
+// They are supported for:
+
+//? Map
+//? Set
+//? Array
+
+// Plain objects also support similar methods, but the syntax is a bit different.
+

b9e0cfdf06baea4a3946bbe246374d6460605410
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 22:49:05 +0500

    feat: add Date.parse from a string.

--- Data-types/Data-time.js
+++ Data-types/Data-time.js
@@ -243,3 +243,30 @@ let end1 = Date.now(); // done
 
 console.log(`The loop took ${end1 - start1} ms`); // subtract numbers, not dates
 
+//* Date.parse from a string
+
+// The method Date.parse(str) can read a date from a string.
+
+// The string format should be: YYYY-MM-DDTHH:mm:ss.sssZ, where:
+
+//? YYYY-MM-DD – is the date: year-month-day.
+//? The character "T" is used as the delimiter.
+//? HH:mm:ss.sss – is the time: hours, minutes, seconds and milliseconds.
+//? The optional 'Z' part denotes the time zone in the format +-hh:mm. A single letter Z would mean UTC+0.
+
+// Shorter variants are also possible, like YYYY-MM-DD or YYYY-MM or even YYYY.
+
+// The call to Date.parse(str) parses the string in the given format and returns the timestamp (number of milliseconds from 1 Jan 1970 UTC+0). If the format is invalid, returns NaN.
+
+// For instance:
+
+let ms = Date.parse("2012-01-26T13:51:50.417-07:00");
+
+console.log(ms); // 1327611110417  (timestamp)
+
+// We can instantly create a new Date object from the timestamp:
+
+let date8 = new Date(Date.parse("2012-01-26T13:51:50.417-07:00"));
+
+console.log(date8);
+

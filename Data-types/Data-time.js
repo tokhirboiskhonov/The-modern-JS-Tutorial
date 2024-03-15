c990345cfd7d7dcb1054a4f1b67ea6eb30668183
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 22:43:33 +0500

    feat: add new Date(milliseconds)

--- Data-types/Data-time.js
+++ Data-types/Data-time.js
@@ -15,3 +15,25 @@
 let now = new Date();
 console.log(now); // shows current date/time
 
+//! new Date(milliseconds)
+
+// Create a Date object with the time equal to number of milliseconds (1/1000 of a second) passed after the Jan 1st of 1970 UTC+0.
+
+// 0 means 01.01.1970 UTC+0
+let Jan01_1970 = new Date(0);
+console.log(Jan01_1970);
+
+// now add 24 hours, get 02.01.1970 UTC+0
+let Jan02_1970 = new Date(24 * 3600 * 1000);
+console.log(Jan02_1970);
+
+// An integer number representing the number of milliseconds that has passed since the beginning of 1970 is called a timestamp.
+
+// It’s a lightweight numeric representation of a date. We can always create a date from a timestamp using new Date(timestamp) and convert the existing Date object to a timestamp using the date.getTime() method (see below).
+
+// Dates before 01.01.1970 have negative timestamps, e.g.:
+
+// 31 Dec 1969
+let Dec31_1969 = new Date(-24 * 3600 * 1000);
+console.log(Dec31_1969);
+

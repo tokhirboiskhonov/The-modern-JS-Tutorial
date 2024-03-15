4f12bad21d5b995a602ba4e59b342447535f0c0e
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 14:09:24 +0500

    feat: add Smart function parameters in destructuring-assignment.

--- Data-types/destructuring-assignment.js
+++ Data-types/destructuring-assignment.js
@@ -386,3 +386,92 @@ console.log(head); // Menu
 // Finally, we have width, height, item1, item2 and title from the default value.
 
 // Note that there are no variables for size and items, as we take their content instead.
+
+//* Smart function parameters
+
+// There are times when a function has many parameters, most of which are optional. That’s especially true for user interfaces. Imagine a function that creates a menu. It may have a width, a height, a title, items list and so on.
+
+// Here’s a bad way to write such a function:
+
+function showMenu(title = "Untitled", width = 200, height = 100, items = []) {
+  // ...
+}
+
+// In real-life, the problem is how to remember the order of arguments. Usually IDEs try to help us, especially if the code is well-documented, but still… Another problem is how to call a function when most parameters are ok by default.
+
+// Like this?
+
+// undefined where default values are fine
+showMenu("My Menu", undefined, undefined, ["Item1", "Item2"]);
+
+//! That’s ugly. And becomes unreadable when we deal with more parameters.
+//? Destructuring comes to the rescue!
+
+// We can pass parameters as an object, and the function immediately destructurizes them into variables:
+
+// we pass object to function
+let objOptions = {
+  title: "My menu",
+  items: ["Item1", "Item2"],
+};
+
+// ...and it immediately expands it to variables
+function showMenu({
+  title = "Untitled",
+  width = 200,
+  height = 100,
+  items = [],
+}) {
+  // title, items – taken from options,
+  // width, height – defaults used
+  console.log(`${title} ${width} ${height}`); // My Menu 200 100
+  console.log(items); // Item1, Item2
+}
+
+showMenu(objOptions);
+
+// We can also use more complex destructuring with nested objects and colon mappings:
+
+let objOptions1 = {
+  title: "My menu",
+  items: ["Item1", "Item2"],
+};
+
+function showMenu({
+  title: t = "Untitled", // title goes to t
+  width: w = 100, // width goes to w
+  height: h = 200, // height goes to h
+  items: [item1, item2], // items first element goes to item1, second to item2
+}) {
+  console.log(`${t} ${w} ${h}`); // My Menu 100 200
+  console.log(item1); // Item1
+  console.log(item2); // Item2
+}
+
+showMenu(objOptions1);
+
+//! The full syntax is the same as for a destructuring assignment:
+
+// function({
+// 	incomingProperty: varName = defaultValue
+// 	...
+//  })
+
+// Then, for an object of parameters, there will be a variable varName for property incomingProperty, with defaultValue by default.
+
+// Please note that such destructuring assumes that showMenu() does have an argument. If we want all values by default, then we should specify an empty object:
+
+showMenu({}); // ok, all values are default
+
+showMenu(); // this would give an error
+
+// We can fix this by making {} the default value for the whole object of parameters:
+
+function showMenu({ title = "Menu", width = 100, height = 200 } = {}) {
+  console.log(`${title} ${width} ${height}`);
+}
+
+showMenu(); // Menu 100 200
+
+// In the code above, the whole arguments object is {} by default, so there’s always something to destructurize.
+

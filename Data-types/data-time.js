856a251b0034131f9e2aa240c639d3d21145557f
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 14:32:09 +0500

    feat: add intro to into Data-time.

---
+++ Data-types/data-time.js
@@ -0,0 +1,5 @@
+//*  Date and time
+
+// Let’s meet a new built-in object: Date. It stores the date, time and provides methods for date/time management.
+
+// For instance, we can use it to store creation/modification times, to measure time, or just to print out the current date.

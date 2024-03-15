d94edbf18296d7226e5d5268395789caebbe44e1
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 11:53:22 +0500

    feat: add intro to into destructuring-assignment.

---
+++ Data-types/destructuring-assignment.js
@@ -0,0 +1,14 @@
+//* Destructuring assignment
+
+// The two most used data structures in JavaScript are Object and Array.
+
+// Objects allow us to create a single entity that stores data items by key.
+
+// Arrays allow us to gather data items into an ordered list.
+
+// However, when we pass these to a function, we may not need all of it. The function might only require certain elements or properties.
+
+// Destructuring assignment is a special syntax that allows us to “unpack” arrays or objects into a bunch of variables, as sometimes that’s more convenient.
+
+// Destructuring also works well with complex functions that have a lot of parameters, default values, and so on. Soon we’ll see that.
+

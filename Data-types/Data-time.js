5b431be41752d1759016472803c076b6c4b26833
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 22:48:46 +0500

    feat: add Date.now()

--- Data-types/Data-time.js
+++ Data-types/Data-time.js
@@ -220,3 +220,26 @@ let end = new Date(); // end measuring time
 
 console.log(`The loop took ${end - start} ms`);
 
+//* Date.now()
+
+// If we only want to measure time, we don’t need the Date object.
+
+// There’s a special method Date.now() that returns the current timestamp.
+
+// It is semantically equivalent to new Date().getTime(), but it doesn’t create an intermediate Date object. So it’s faster and doesn’t put pressure on garbage collection.
+
+// It is used mostly for convenience or when performance matters, like in games in JavaScript or other specialized applications.
+
+// So this is probably better:
+
+let start1 = Date.now(); // milliseconds count from 1 Jan 1970
+
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+
+let end1 = Date.now(); // done
+
+console.log(`The loop took ${end1 - start1} ms`); // subtract numbers, not dates
+

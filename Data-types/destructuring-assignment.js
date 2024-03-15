995fbae047f3b2c7b213f13a373ed5dc6c5b248e
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 14:09:51 +0500

    feat: add Summary to into a destructuring-assignment.

--- Data-types/destructuring-assignment.js
+++ Data-types/destructuring-assignment.js
@@ -475,3 +475,23 @@ showMenu(); // Menu 100 200
 
 // In the code above, the whole arguments object is {} by default, so there’s always something to destructurize.
 
+//* Summary
+
+// Destructuring assignment allows for instantly mapping an object or array onto many variables.
+
+//? The full object syntax:
+
+//? let {prop : varName = default, ...rest} = object
+
+// This means that property prop should go into the variable varName and, if no such property exists, then the default value should be used.
+
+// Object properties that have no mapping are copied to the rest object.
+
+//? The full array syntax:
+
+//? let [item1 = default, item2, ...rest] = array
+
+// The first item goes to item1; the second goes into item2, all the rest makes the array rest.
+
+// It’s possible to extract data from nested arrays/objects, for that the left side must have the same structure as the right one.
+

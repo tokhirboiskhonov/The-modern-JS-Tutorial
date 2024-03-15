7d56008019c9aa0c97de837a047150c3a21817dd
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 22:48:30 +0500

    feat: Date to number, date diff.

--- Data-types/Data-time.js
+++ Data-types/Data-time.js
@@ -197,3 +197,26 @@ console.log(date6);
 date6.setDate(0); // min day is 1, so the last day of the previous month is assumed
 console.log(date6); // 31 Dec 2015
 
+//* Date to number, date diff
+
+// When a Date object is converted to number, it becomes the timestamp same as date.getTime():
+
+let date7 = new Date();
+
+alert(+date7); // the number of milliseconds, same as date.getTime()
+
+// The important side effect: dates can be subtracted, the result is their difference in ms.
+
+// That can be used for time measurements:
+
+let start = new Date(); // start measuring time
+
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+
+let end = new Date(); // end measuring time
+
+console.log(`The loop took ${end - start} ms`);
+

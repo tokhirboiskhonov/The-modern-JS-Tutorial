fabbe96a0930c2c6efcaee471b039600bf9a3d95
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-16 01:27:33 +0500

    feat: add summary to into JSON topic.

--- Data-types/jsonMethods.js
+++ Data-types/jsonMethods.js
@@ -412,4 +412,16 @@ schedule = JSON.parse(schedule, function (key, value) {
 
 alert(schedule.meetups[1].date.getDate()); // works!
 
+//* Summary
+
+//? JSON is a data format that has its own independent standard and libraries for most programming languages.
+
+//? JSON supports plain objects, arrays, strings, numbers, booleans, and null.
+
+//? JavaScript provides methods JSON.stringify to serialize into JSON and JSON.parse to read from JSON.
+
+//? Both methods support transformer functions for smart reading/writing.
+
+//? If an object has toJSON, then it is called by JSON.stringify.
+
 

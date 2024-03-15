5a104e8dfa97c3ecdf3e9a1b7684fcb63fc2c9f7
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 13:03:35 +0500

    feat: add Object destructuring to into a destructuring-assignment topic.

--- Data-types/destructuring-assignment.js
+++ Data-types/destructuring-assignment.js
@@ -194,3 +194,108 @@ console.log(name); // Julius (from array)
 console.log(surname3); // whatever prompt gets
 
 //? Please note: the prompt will run only for the missing value (surname).
+
+//* Object destructuring
+
+// The destructuring assignment also works with objects.
+
+// The basic syntax is:
+
+//? let {var1, var2} = {var1:…, var2:…}
+
+// We should have an existing object on the right side, that we want to split into variables. The left side contains an object-like “pattern” for corresponding properties. In the simplest case, that’s a list of variable names in {...}.
+
+// For instance:
+
+let options = {
+  titles: "Menu",
+  width: 100,
+  height: 200,
+};
+
+let { titles, width, height } = options;
+
+console.log(titles); // Menu
+console.log(width); // 100
+console.log(height); // 200
+
+// Properties options.titles, options.width and options.height are assigned to the corresponding variables.
+
+// The order does not matter. This works too:
+
+// changed the order in let {...}
+let { height2, width2, title2 } = { title: "Menu", height: 200, width: 100 };
+
+// The pattern on the left side may be more complex and specify the mapping between properties and variables.
+
+// If we want to assign a property to a variable with another name, for instance, make options.width go into the variable named w, then we can set the variable name using a colon:
+
+let options1 = {
+  title: "Menu",
+  width: 100,
+  height: 200,
+};
+
+// { sourceProperty: targetVariable }
+let { width: w, height: h, titlea } = options1;
+
+// width -> w
+// height -> h
+// title -> title
+
+console.log(titlea); // Menu
+console.log(w); // 100
+console.log(h); // 200
+
+// The colon shows “what : goes where”. In the example above the property width goes to w, property height goes to h, and title is assigned to the same name.
+
+// For potentially missing properties we can set default values using "=", like this:
+
+let options2 = {
+  title: "Menu",
+};
+
+let { width3 = 100, height3 = 200, title3 } = options2;
+
+console.log(title3); // Menu
+console.log(width3); // 100
+console.log(height3); // 200
+
+// Just like with arrays or function parameters, default values can be any expressions or even function calls. They will be evaluated if the value is not provided.
+
+// In the code below prompt asks for width, but not for title:
+
+let option3 = {
+  title: "Menu",
+};
+
+let { width4 = prompt("width?"), title4 = prompt("title?") } = options3;
+
+console.log(title4); // Menu
+console.log(width4); // (whatever the result of prompt is)
+
+// We also can combine both the colon and equality:
+
+let options4 = {
+  title: "Menu",
+};
+
+let { width: w1 = 100, height: h1 = 200, title5 } = options4;
+
+console.log(titl5); // Menu
+console.log(w1); // 100
+console.log(h1); // 200
+
+// If we have a complex object with many properties, we can extract only what we need:
+
+let options5 = {
+  title: "Menu",
+  width: 100,
+  height: 200,
+};
+
+// only extract title as a variable
+let { title6 } = options5;
+
+console.log(title6); // Menu
+

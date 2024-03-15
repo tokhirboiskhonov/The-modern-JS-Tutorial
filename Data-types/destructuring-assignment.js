af90247e062341f162780ab09da2a6e5b1cf30ed
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-15 11:53:50 +0500

    feat: add Array destructuring.

--- Data-types/destructuring-assignment.js
+++ Data-types/destructuring-assignment.js
@@ -12,3 +12,28 @@
 
 // Destructuring also works well with complex functions that have a lot of parameters, default values, and so on. Soon we’ll see that.
 
+//* Array destructuring
+
+// Here's an example of how an array is destructured into variables:
+
+let arr = ["John", "Smith"];
+
+// destructuring assignment
+// sets firstName = arr[0]
+// and surname = arr[1]
+let [firstName, lastName] = arr;
+
+console.log(firstName); //John
+console.log(lastName); //Smith
+
+// Now we can work with variables instead of array members.
+
+// It looks great when combined with split or other array-returning methods:
+
+let [currentName, surname] = "John Smith".split(" ");
+
+console.log(currentName); // John
+console.log(surname); // Smith
+
+// As you can see, the syntax is simple. There are several peculiar details though. Let’s see more examples to understand it better.
+

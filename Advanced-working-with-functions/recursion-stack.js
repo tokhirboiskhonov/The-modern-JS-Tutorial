301a0d6f6780e91558c55c40115e56a7339b5bab
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-30 22:40:50 +0500

    feat: add the execution context and stack

--- Advanced-working-with-functions/recursion-stack.js
+++ Advanced-working-with-functions/recursion-stack.js
@@ -85,3 +85,22 @@ alert(pow(2, 3)); // 8
 
 // That limits the application of recursion, but it still remains very wide. There are many tasks where recursive way of thinking gives simpler code, easier to maintain.
 
+//* The execution context and stack
+
+// Now let’s examine how recursive calls work. For that we’ll look under the hood of functions.
+
+// The information about the process of execution of a running function is stored in its execution context.
+
+// The execution context is an internal data structure that contains details about the execution of a function: where the control flow is now, the current variables, the value of this (we don’t use it here) and few other internal details.
+
+// One function call has exactly one execution context associated with it.
+
+// When a function makes a nested call, the following happens:
+
+//? The current function is paused.
+//? The execution context associated with it is remembered in a special data structure called execution context stack.
+//? The nested call executes.
+//? After it ends, the old execution context is retrieved from the stack, and the outer function is resumed from where it stopped.
+
+// Let’s see what happens during the pow(2, 3) call.
+

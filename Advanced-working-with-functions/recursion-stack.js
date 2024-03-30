08ff6b6c27a97299a6127823160c52156f67d8af
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-30 22:45:31 +0500

    feat: add Summary to into the topic.

--- Advanced-working-with-functions/recursion-stack.js
+++ Advanced-working-with-functions/recursion-stack.js
@@ -399,3 +399,22 @@ list1.next.next.next.next = null;
 let secondList = list1.next.next;
 list1.next.next = null;
 
+//* Summary
+
+//! Terms:
+
+// Recursion is a programming term that means calling a function from itself. Recursive functions can be used to solve tasks in elegant ways.
+
+// When a function calls itself, that’s called a recursion step. The basis of recursion is function arguments that make the task so simple that the function does not make further calls.
+
+// A recursively-defined data structure is a data structure that can be defined using itself.
+
+// For instance, the linked list can be defined as a data structure consisting of an object referencing a list (or null).
+
+// list = { value, next -> list }
+
+// Trees like HTML elements tree or the department tree from this chapter are also naturally recursive: they have branches and every branch can have other branches.
+
+// Recursive functions can be used to walk them as we’ve seen in the sumSalary example.
+
+// Any recursive function can be rewritten into an iterative one. And that’s sometimes required to optimize stuff. But for many tasks a recursive solution is fast enough and easier to write and support.

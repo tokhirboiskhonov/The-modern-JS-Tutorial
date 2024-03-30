be64b1f5bda459ef3750fc406caa824a5df7c66e
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-30 23:53:55 +0500

    feat: add intro for rest param and spread syntax.

---
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -0,0 +1,12 @@
+//* Rest parameters and spread syntax
+
+// Many JavaScript built-in functions support an arbitrary number of arguments.
+
+// For instance:
+
+//? Math.max(arg1, arg2, ..., argN) – returns the greatest of the arguments.
+//? Object.assign(dest, src1, ..., srcN) – copies properties from src1..N into dest.
+//? …and so on.
+
+// In this chapter we’ll learn how to do the same. And also, how to pass arrays to such functions as parameters.
+

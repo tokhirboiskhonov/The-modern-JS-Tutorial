fbf50a961af0838b28e9b8515e6f03d853a78989
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-30 23:55:20 +0500

    update: The rest parameters must be at the end. Otherwise it will be error.

--- Advanced-working-with-functions/rest-parameters--spread-syntax.js
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -57,3 +57,11 @@ function showName(firstName, lastName, ...titles) {
 
 showName("Julius", "Caesar", "Consul", "Imperator");
 
+//! The rest parameters must be at the end
+
+// The rest parameters gather all remaining arguments, so the following does not make sense and causes an error:
+
+// function f(arg1, ...rest, arg2) { // arg2 after ...rest ?!
+// error
+// }
+// The ...rest must always be last.

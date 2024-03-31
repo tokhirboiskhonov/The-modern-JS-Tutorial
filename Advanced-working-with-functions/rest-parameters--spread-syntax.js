b8bd73c752c04f453d6105f4df78a228a7e96526
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-31 05:37:48 +0500

    ! remained: Arrow functions do not have "arguments"

--- Advanced-working-with-functions/rest-parameters--spread-syntax.js
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -95,3 +95,18 @@ showName("Ilya");
 
 // So when we need these features, then rest parameters are preferred.
 
+//! Arrow functions do not have "arguments"
+
+// If we access the arguments object from an arrow function, it takes them from the outer “normal” function.
+
+// Here’s an example:
+
+function f() {
+  let showArg = () => alert(arguments[0]);
+  showArg();
+}
+
+f(1); // 1
+
+// As we remember, arrow functions don’t have their own this. Now we know they don’t have the special arguments object either.
+

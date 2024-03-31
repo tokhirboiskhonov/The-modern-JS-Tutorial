453b3bd1c041fd7035c81079f2c3d932564126dd
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-31 05:39:39 +0500

    feat: add summary to into the topic.

--- Advanced-working-with-functions/rest-parameters--spread-syntax.js
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -233,3 +233,20 @@ alert(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
 
 // This way of copying an object is much shorter than let objCopy = Object.assign({}, obj) or for an array let arrCopy = Object.assign([], arr) so we prefer to use it whenever we can.
 
+//* Summary
+
+// When we see "..." in the code, it is either rest parameters or the spread syntax.
+
+//! There’s an easy way to distinguish between them:
+
+//? When ... is at the end of function parameters, it’s “rest parameters” and gathers the rest of the list of arguments into an array.
+//? When ... occurs in a function call or alike, it’s called a “spread syntax” and expands an array into a list.
+
+//! Use patterns:
+
+//? Rest parameters are used to create functions that accept any number of arguments.
+//? The spread syntax is used to pass an array to functions that normally require a list of many arguments.
+
+// Together they help to travel between a list and an array of parameters with ease.
+
+// All arguments of a function call are also available in “old-style” arguments: array-like iterable object.

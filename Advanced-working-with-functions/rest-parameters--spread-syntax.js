7a11a13cf1bb4c8819fa9ed595e530282a1f6986
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-31 05:39:15 +0500

    feat: add copy an array/object

--- Advanced-working-with-functions/rest-parameters--spread-syntax.js
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -191,3 +191,45 @@ alert(Array.from(str2)); // H,e,l,l,o
 
 // So, for the task of turning something into an array, Array.from tends to be more universal.
 
+//* Copy an array/object
+
+// Remember when we talked about Object.assign()?
+
+// It is possible to do the same thing with the spread syntax.
+
+let arr7 = [1, 2, 3];
+
+let arrCopy = [...arr7]; // spread the array into a list of parameters
+// then put the result into a new array
+
+// do the arrays have the same contents?
+alert(JSON.stringify(arr7) === JSON.stringify(arrCopy)); // true
+
+// are the arrays equal?
+alert(arr7 === arrCopy); // false (not same reference)
+
+// modifying our initial array does not modify the copy:
+arr7.push(4);
+alert(arr7); // 1, 2, 3, 4
+alert(arrCopy); // 1, 2, 3
+
+// Note that it is possible to do the same thing to make a copy of an object:
+
+let obj = { a: 1, b: 2, c: 3 };
+
+let objCopy = { ...obj }; // spread the object into a list of parameters
+// then return the result in a new object
+
+// do the objects have the same contents?
+alert(JSON.stringify(obj) === JSON.stringify(objCopy)); // true
+
+// are the objects equal?
+alert(obj === objCopy); // false (not same reference)
+
+// modifying our initial object does not modify the copy:
+obj.d = 4;
+alert(JSON.stringify(obj)); // {"a":1,"b":2,"c":3,"d":4}
+alert(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
+
+// This way of copying an object is much shorter than let objCopy = Object.assign({}, obj) or for an array let arrCopy = Object.assign([], arr) so we prefer to use it whenever we can.
+

bd3ca5c4322cdf8ab1f920c1c84b698d2dc19857
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-31 05:37:04 +0500

    feat: add the "arguments" variable.

--- Advanced-working-with-functions/rest-parameters--spread-syntax.js
+++ Advanced-working-with-functions/rest-parameters--spread-syntax.js
@@ -65,3 +65,33 @@ showName("Julius", "Caesar", "Consul", "Imperator");
 // error
 // }
 // The ...rest must always be last.
+
+//* The “arguments” variable
+//
+// There is also a special array-like object named arguments that contains all arguments by their index.
+
+// For instance:
+
+function showName() {
+  alert(arguments.length);
+  alert(arguments[0]);
+  alert(arguments[1]);
+
+  // it's iterable
+  // for(let arg of arguments) alert(arg);
+}
+
+// shows: 2, Julius, Caesar
+showName("Julius", "Caesar");
+
+// shows: 1, Ilya, undefined (no second argument)
+showName("Ilya");
+
+// In old times, rest parameters did not exist in the language, and using arguments was the only way to get all arguments of the function. And it still works, we can find it in the old code.
+
+// But the downside is that although arguments is both array-like and iterable, it’s not an array. It does not support array methods, so we can’t call arguments.map(...) for example.
+
+// Also, it always contains all arguments. We can’t capture them partially, like we did with rest parameters.
+
+// So when we need these features, then rest parameters are preferred.
+

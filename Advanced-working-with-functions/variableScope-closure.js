a499a5a1f46533a64bb63abce5d7c07a79198aa8
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 00:29:53 +0500

    feat: add intro to into our project

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -1,3 +1,22 @@
+//* Variable scope, closure
+
+// JavaScript is a very function-oriented language. It gives us a lot of freedom. A function can be created at any moment, passed as an argument to another function, and then called from a totally different place of code later.
+
+// We already know that a function can access variables outside of it (“outer” variables).
+
+// But what happens if outer variables change since a function is created? Will the function get newer values or the old ones?
+
+// And what if a function is passed along as an argument and called from another place of code, will it get access to outer variables at the new place?
+
+// Let’s expand our knowledge to understand these scenarios and more complex ones.
+
+//? We’ll talk about let/const variables here
+// In JavaScript, there are 3 ways to declare a variable: let, const (the modern ones), and var (the remnant of the past).
+
+// In this article we’ll use let variables in examples.
+// Variables, declared with const, behave the same, so this article is about const too.
+// The old var has some notable differences, they will be covered in the article The old "var".
+
 //* Tasks
 
 //! 1. Does a function pickup latest changes?

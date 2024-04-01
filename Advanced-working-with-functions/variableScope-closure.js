2f5a7a08c22224867fc577d1652ac4cc1755ef67
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-01 22:31:43 +0500

    feat: add 1st task Does a function pickup latest changes?

---
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -0,0 +1,14 @@
+//* Tasks
+
+//! 1. Does a function pickup latest changes?
+
+// let name = "John";
+
+// function sayHi() {
+//   console.log("Hi, " + name);
+// }
+
+// name = "Pete";
+
+// sayHi(); // Hi, Pete
+

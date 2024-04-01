1822376d5ee8473a0e1b09722e5d02709bf6008d
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 00:04:22 +0500

    feat: add last task is about Army of functions.

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -146,3 +146,29 @@ console.log(users); // Output: [{ name: "Ann", age: 19, surname: "Hathaway" }, {
 users.sort(byField("age"));
 console.log(users);
 
+//! 10. Army of functions
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let j = i;
+    let shooter = function () {
+      // create a shooter function,
+      console.log(j); // that should show its number
+    };
+    shooters.push(shooter); // and add it to the array
+    i++;
+  }
+
+  // ...and return the array of shooters
+  return shooters;
+}
+
+let army = makeArmy();
+
+// all shooters show 10 instead of their numbers 0, 1, 2, 3...
+army[0](); // 10 from the shooter number 0
+army[1](); // 10 from the shooter number 1
+army[2]();

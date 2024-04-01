9b74296739488277eb3db0544eef69cd90c02e7f
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 00:03:09 +0500

    feat: add 8th task is about Filter through function

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -105,3 +105,24 @@ function func() {
 
 func();
 
+//! 8. Filter through function
+
+/* .. your code for inBetween and inArray */
+
+function inBetween(a, b) {
+  return function (elem) {
+    return elem >= a && elem <= b;
+  };
+}
+
+function inArray(arr) {
+  return function (el) {
+    return arr.includes(el);
+  };
+}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
+
+console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
+

8b8e30109659b35d346e2de87d098a7916be3aef
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-01 22:32:06 +0500

    feat: add 2nd task is which variables are available?

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -12,3 +12,21 @@
 
 // sayHi(); // Hi, Pete
 
+//! 2. Which variables are available?
+
+function makeWorker() {
+  let name = "Pete";
+
+  return function () {
+    console.log(name);
+  };
+}
+
+let name = "John";
+
+// create a function
+let work = makeWorker();
+
+// call it
+work(); // Pete
+

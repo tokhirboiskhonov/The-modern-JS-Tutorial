5c83c8661698cbf3b44f86368fcf986eb2a8621a
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-01 22:32:30 +0500

    feat: add 3rd task is Are counters independent?

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -30,3 +30,22 @@ let work = makeWorker();
 // call it
 work(); // Pete
 
+//! 3. Are counters independent?
+
+// function makeCounter() {
+//   let count = 0;
+
+//   return function () {
+//     return count++;
+//   };
+// }
+
+// let counter = makeCounter();
+// let counter2 = makeCounter();
+
+// console.log(counter()); // 0
+// console.log(counter()); // 1
+
+// console.log(counter2()); // 0
+// console.log(counter2()); // 1
+

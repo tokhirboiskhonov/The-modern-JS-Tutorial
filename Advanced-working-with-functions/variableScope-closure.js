1bb57a6b3c7bb13a6fca0a56bcffb8b0d08b1fe4
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 00:31:28 +0500

    feat: add nested function

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -84,6 +84,50 @@ console.log(i); // Error, no such variable
 
 // Visually, let i is outside of {...}. But the for construct is special here: the variable, declared inside it, is considered a part of the block.
 
+//* Nested functions
+
+// A function is called “nested” when it is created inside another function.
+
+// It is easily possible to do this with JavaScript.
+
+// We can use it to organize our code, like this:
+
+function sayHiBye(firstName, lastName) {
+  // helper nested function to use below
+  function getFullName() {
+    return firstName + " " + lastName;
+  }
+
+  console.log("Hello, " + getFullName());
+  console.log("Bye, " + getFullName());
+}
+
+// Here the nested function getFullName() is made for convenience. It can access the outer variables and so can return the full name. Nested functions are quite common in JavaScript.
+
+// What’s much more interesting, a nested function can be returned: either as a property of a new object or as a result by itself. It can then be used somewhere else. No matter where, it still has access to the same outer variables.
+
+// Below, makeCounter creates the “counter” function that returns the next number on each invocation:
+
+function makeCounter() {
+  let count = 0;
+
+  return function () {
+    return count++;
+  };
+}
+
+let counter1 = makeCounter();
+
+alert(counter1()); // 0
+alert(counter1()); // 1
+alert(counter1()); // 2
+
+// Despite being simple, slightly modified variants of that code have practical uses, for instance, as a random number generator to generate random values for automated tests.
+
+// How does this work? If we create multiple counters, will they be independent? What’s going on with the variables here?
+
+// Understanding such things is great for the overall knowledge of JavaScript and beneficial for more complex scenarios. So let’s go a bit in-depth.
+
 
 //* Tasks
 

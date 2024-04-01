1a19523e9a0cd2848196935ce12d9d8a0dd2b7cc
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 00:31:05 +0500

    feat: add code blocks

--- Advanced-working-with-functions/variableScope-closure.js
+++ Advanced-working-with-functions/variableScope-closure.js
@@ -17,6 +17,74 @@
 // Variables, declared with const, behave the same, so this article is about const too.
 // The old var has some notable differences, they will be covered in the article The old "var".
 
+//* Code blocks
+
+// If a variable is declared inside a code block {...}, it’s only visible inside that block.
+
+// For example:
+
+{
+  // do some job with local variables that should not be seen outside
+
+  let message = "Hello"; // only visible in this block
+
+  console.log(message); // Hello
+}
+
+console.log(message); // Error: message is not defined
+
+// We can use this to isolate a piece of code that does its own task, with variables that only belong to it:
+
+{
+  // show message
+  let message = "Hello";
+  console.log(message);
+}
+
+{
+  // show another message
+  let message = "Goodbye";
+  console.log(message);
+}
+
+//? There’d be an error without blocks
+
+// Please note, without separate blocks there would be an error, if we use let with the existing variable name:
+
+// show message
+// let message = "Hello";
+// console.log(message);
+
+// show another message
+// let message = "Goodbye"; // Error: variable already declared
+// console.log(message);
+
+// For if, for, while and so on, variables declared in {...} are also only visible inside:
+
+if (true) {
+  let phrase = "Hello!";
+
+  console.log(phrase); // Hello!
+}
+
+console.log(phrase); // Error, no such variable!
+
+// Here, after if finishes, the alert below won’t see the phrase, hence the error.
+
+// That’s great, as it allows us to create block-local variables, specific to an if branch.
+
+// The similar thing holds true for for and while loops:
+
+for (let i = 0; i < 3; i++) {
+  // the variable i is only visible inside this for
+  console.log(i); // 0, then 1, then 2
+}
+
+console.log(i); // Error, no such variable
+
+// Visually, let i is outside of {...}. But the for construct is special here: the variable, declared inside it, is considered a part of the block.
+
+
 //* Tasks
 
 //! 1. Does a function pickup latest changes?

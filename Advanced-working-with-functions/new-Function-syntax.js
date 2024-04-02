667e2a875c8df5808fbf038028501862a863ecad
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 21:37:26 +0500

    feat: add Summary to into a topic.

--- Advanced-working-with-functions/new-Function-syntax.js
+++ Advanced-working-with-functions/new-Function-syntax.js
@@ -87,3 +87,18 @@ getFunc()(); // "test", from the Lexical Environment of getFunc
 
 // To pass something to a function, created as new Function, we should use its arguments.
 
+//* Summary
+
+// The syntax:
+
+// let func = new Function ([arg1, arg2, ...argN], functionBody);
+
+// For historical reasons, arguments can also be given as a comma-separated list.
+
+// These three declarations mean the same:
+
+new Function("a", "b", "return a + b"); // basic syntax
+new Function("a,b", "return a + b"); // comma-separated
+new Function("a , b", "return a + b"); // comma-separated with spaces
+
+// Functions created with new Function, have [[Environment]] referencing the global Lexical Environment, not the outer one. Hence, they cannot use outer variables. But that’s actually good, because it insures us from errors. Passing parameters explicitly is a much better method architecturally and causes no problems with minifiers.

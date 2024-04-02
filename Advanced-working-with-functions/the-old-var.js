5c19f1e23bec5e9b34b4f3df496a880f4c7895b5
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 16:30:34 +0500

    feat: add "var" has no block scope.

--- Advanced-working-with-functions/the-old-var.js
+++ Advanced-working-with-functions/the-old-var.js
@@ -23,3 +23,50 @@ alert(message); // Hi
 
 // On the other hand, it’s important to understand differences when migrating old scripts from var to let, to avoid odd errors.
 
+//* “var” has no block scope
+
+// Variables, declared with var, are either function-scoped or global-scoped. They are visible through blocks.
+
+// For instance:
+
+if (true) {
+  var test = true; // use "var" instead of "let"
+}
+
+alert(test); // true, the variable lives after if
+
+// As var ignores code blocks, we’ve got a global variable test.
+
+// If we used let test instead of var test, then the variable would only be visible inside if:
+
+if (true) {
+  let test = true; // use "let"
+}
+
+alert(test); // ReferenceError: test is not defined
+
+// The same thing for loops: var cannot be block- or loop-local:
+
+for (var i = 0; i < 10; i++) {
+  var one = 1;
+  // ...
+}
+
+alert(i); // 10, "i" is visible after loop, it's a global variable
+alert(one); // 1, "one" is visible after loop, it's a global variable
+
+// If a code block is inside a function, then var becomes a function-level variable:
+
+function sayHi() {
+  if (true) {
+    var phrase = "Hello";
+  }
+
+  alert(phrase); // works
+}
+
+sayHi();
+alert(phrase); // ReferenceError: phrase is not defined
+
+// As we can see, var pierces through if, for or other code blocks. That’s because a long time ago in JavaScript, blocks had no Lexical Environments, and var is a remnant of that.
+

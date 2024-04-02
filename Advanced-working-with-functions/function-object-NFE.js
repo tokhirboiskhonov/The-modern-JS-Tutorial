3683ee3bfe1e0e30f29808bf45335a469c19987a
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 21:16:31 +0500

    update: The "length" property

--- Advanced-working-with-functions/function-object-NFE.js
+++ Advanced-working-with-functions/function-object-NFE.js
@@ -61,3 +61,53 @@ let arr = [function () {}];
 alert(arr[0].name); // <empty string>
 // the engine has no way to set up the right name, so there is none
 // In practice, however, most functions do have a name.
+
+//* The "length" property
+
+// There is another built-in property “length” that returns the number of function parameters, for instance:
+
+function f1(a) {}
+function f2(a, b) {}
+function many(a, b, ...more) {}
+
+alert(f1.length); // 1
+alert(f2.length); // 2
+alert(many.length); // 2
+
+// Here we can see that rest parameters are not counted.
+
+// The length property is sometimes used for introspection in functions that operate on other functions.
+
+// For instance, in the code below the ask function accepts a question to ask and an arbitrary number of handler functions to call.
+
+// Once a user provides their answer, the function calls the handlers. We can pass two kinds of handlers:
+
+//? *A zero-argument function, which is only called when the user gives a positive answer.
+//? *A function with arguments, which is called in either case and returns an answer.
+
+// To call handler the right way, we examine the handler.length property.
+
+// The idea is that we have a simple, no-arguments handler syntax for positive cases (most frequent variant), but are able to support universal handlers as well:
+
+function ask(question, ...handlers) {
+  let isYes = confirm(question);
+
+  for (let handler of handlers) {
+    if (handler.length == 0) {
+      if (isYes) handler();
+    } else {
+      handler(isYes);
+    }
+  }
+}
+
+// for positive answer, both handlers are called
+// for negative answer, only the second one
+ask(
+  "Question?",
+  () => alert("You said yes"),
+  (result) => alert(result)
+);
+
+//  This is a particular case of so-called polymorphism – treating arguments differently depending on their type or, in our case depending on the length. The idea does have a use in JavaScript libraries.
+

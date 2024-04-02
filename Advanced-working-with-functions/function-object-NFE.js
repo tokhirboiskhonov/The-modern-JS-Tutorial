8e2716fd2468fa04fd986f26f06e654b16a2b358
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 18:41:27 +0500

    feat: add intro about Function object, NFE.

---
+++ Advanced-working-with-functions/function-object-NFE.js
@@ -0,0 +1,10 @@
+//* Function object, NFE
+
+// As we already know, a function in JavaScript is a value.
+
+// Every value in JavaScript has a type. What type is a function?
+
+// In JavaScript, functions are objects.
+
+// A good way to imagine functions is as callable “action objects”. We can not only call them, but also treat them as objects: add/remove properties, pass by reference etc.
+

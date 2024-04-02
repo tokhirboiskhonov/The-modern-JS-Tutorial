dec94041cf1c00e606b87c064cd3d3973db25247
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 18:34:51 +0500

    feat: add summary to into the topic.

--- Advanced-working-with-functions/global-object.js
+++ Advanced-working-with-functions/global-object.js
@@ -61,3 +61,18 @@ if (!window.Promise) {
 //   window.Promise = ... // custom implementation of the modern language feature
 // }
 
+//* Summary
+
+//? 1. The global object holds variables that should be available everywhere.
+
+// That includes JavaScript built-ins, such as Array and environment-specific values, such as window.innerHeight – the window height in the browser.
+
+//? 2. The global object has a universal name globalThis.
+
+// …But more often is referred by “old-school” environment-specific names, such as window (browser) and global (Node.js).
+
+//? 3. We should store values in the global object only if they’re truly global for our project. And keep their number at minimum.
+
+//? 4. In-browser, unless we’re using modules, global functions and variables declared with var become a property of the global object.
+
+//? 5. To make our code future-proof and easier to understand, we should access properties of the global object directly, as window.x.

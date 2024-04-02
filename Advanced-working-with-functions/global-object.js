0ab124ad663918eddedb66195f2df9bf7d7f8a7b
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 18:34:28 +0500

    feat: explain to how to using for polyfills.

--- Advanced-working-with-functions/global-object.js
+++ Advanced-working-with-functions/global-object.js
@@ -46,3 +46,18 @@ alert(window.currentUser.name); // John
 
 // That said, using global variables is generally discouraged. There should be as few global variables as possible. The code design where a function gets “input” variables and produces certain “outcome” is clearer, less prone to errors and easier to test than if it uses outer or global variables.
 
+//* Using for polyfills
+
+// We use the global object to test for support of modern language features.
+
+// For instance, test if a built-in Promise object exists (it doesn’t in really old browsers):
+
+if (!window.Promise) {
+  alert("Your browser is really old!");
+}
+// If there’s none (say, we’re in an old browser), we can create “polyfills”: add functions that are not supported by the environment, but exist in the modern standard.
+
+// if (!window.Promise) {
+//   window.Promise = ... // custom implementation of the modern language feature
+// }
+

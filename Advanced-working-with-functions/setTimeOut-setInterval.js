6906bc8e424abced691de29be4724f9f89d66ff0
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-04 23:50:37 +0500

    feat: add setTimeOut to into the topic.

--- Advanced-working-with-functions/setTimeOut-setInterval.js
+++ Advanced-working-with-functions/setTimeOut-setInterval.js
@@ -9,3 +9,57 @@
 
 // These methods are not a part of JavaScript specification. But most environments have the internal scheduler and provide these methods. In particular, they are supported in all browsers and Node.js.
 
+//* setTimeout
+
+//! The syntax:
+
+//? let timerId = setTimeout(func|code, [delay], [arg1], [arg2], ...)
+
+// Parameters:
+
+// func|code
+
+// Function or a string of code to execute. Usually, that’s a function. For historical reasons, a string of code can be passed, but that’s not recommended.
+
+// delay
+
+// The delay before run, in milliseconds (1000 ms = 1 second), by default 0.
+
+// arg1, arg2…
+
+// Arguments for the function
+
+// For instance, this code calls sayHi() after one second:
+
+function sayHi() {
+  alert("Hello");
+}
+
+setTimeout(sayHi, 1000);
+
+// With arguments:
+
+function sayHi(phrase, who) {
+  alert(phrase + ", " + who);
+}
+
+setTimeout(sayHi, 1000, "Hello", "John"); // Hello, John
+
+//  If the first argument is a string, then JavaScript creates a function from it.
+
+//  So, this will also work:
+
+setTimeout("alert('Hello')", 1000);
+
+//  But using strings is not recommended, use arrow functions instead of them, like this:
+
+setTimeout(() => alert("Hello"), 1000);
+
+//! Pass a function, but don’t run it
+// Novice developers sometimes make a mistake by adding brackets () after the function:
+
+// wrong!
+setTimeout(sayHi(), 1000);
+
+// That doesn’t work, because setTimeout expects a reference to a function. And here sayHi() runs the function, and the result of its execution is passed to setTimeout. In our case the result of sayHi() is undefined (the function returns nothing), so nothing is scheduled.
+

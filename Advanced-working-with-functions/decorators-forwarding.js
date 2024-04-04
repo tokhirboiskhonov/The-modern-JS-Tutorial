cedbeab99bf1d2c56a98eea8ba650cf55e46ba52
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-05 00:01:38 +0500

    feat: add introduction is abotu decorators and forwarding to into the topic.

---
+++ Advanced-working-with-functions/decorators-forwarding.js
@@ -0,0 +1,3 @@
+//* Decorators and forwarding, call/apply
+
+// JavaScript gives exceptional flexibility when dealing with functions. They can be passed around, used as objects, and now we’ll see how to forward calls between them and decorate them.

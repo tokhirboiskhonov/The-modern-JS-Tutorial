21cc2a742686be781d02a8933e48bdee167e2bd0
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-04 23:51:05 +0500

    feat: explain that how to canceling with clearTimeout.

--- Advanced-working-with-functions/setTimeOut-setInterval.js
+++ Advanced-working-with-functions/setTimeOut-setInterval.js
@@ -63,3 +63,26 @@ setTimeout(sayHi(), 1000);
 
 // That doesn’t work, because setTimeout expects a reference to a function. And here sayHi() runs the function, and the result of its execution is passed to setTimeout. In our case the result of sayHi() is undefined (the function returns nothing), so nothing is scheduled.
 
+//* Canceling with clearTimeout
+
+// A call to setTimeout returns a “timer identifier” timerId that we can use to cancel the execution.
+
+// The syntax to cancel:
+
+//? * let timerId = setTimeout(...);
+
+clearTimeout(timerId);
+// In the code below, we schedule the function and then cancel it (changed our mind). As a result, nothing happens:
+
+let timerId = setTimeout(() => alert("never happens"), 1000);
+alert(timerId); // timer identifier
+
+clearTimeout(timerId);
+alert(timerId); // same identifier (doesn't become null after canceling)
+
+// As we can see from alert output, in a browser the timer identifier is a number. In other environments, this can be something else. For instance, Node.js returns a timer object with additional methods.
+
+// Again, there is no universal specification for these methods, so that’s fine.
+
+// For browsers, timers are described in the timers section of HTML Living Standard.
+

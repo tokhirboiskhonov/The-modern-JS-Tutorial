1caf862120f1339ebe9be6c96219e05527056a52
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-10 22:29:39 +0500

    functtion/variable(binding) declaration, initialization, assignment, reassignment.

---
+++ js-demo.js
@@ -0,0 +1,23 @@
+//* Function/variable(binding) declaration, initialization, assignment, reassignment
+
+function f() {
+  {
+    let a = 1;
+    var b = 2;
+  }
+
+  //   console.log(a); // Reference error, because of scope
+  console.log(b);
+}
+
+f();
+
+// const a = 1; //Initilizate // nearest block scope;
+
+// let b; //nearest block scope; //Initilizate //! if we don't give the value, it will be automatically undefined
+
+// b = 2; //assign
+// b = 3; //reassign
+
+// var c = 4; // nearest function scope;
+

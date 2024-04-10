63f98279103574b60864b9f9f37301d2c3983841
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-10 22:31:23 +0500

    Math operators

--- js-demo.js
+++ js-demo.js
@@ -82,3 +82,20 @@ console.log(n); // 0
 // ++ and -- operands only work with variable, we can't use straightly with value, For instance: you can write variable++ or variable-- to increment or decrement the value of variable by 1, but you cannot write 5++ or 10-- directly.
 // console.log(10++); // SyntaxError: Invalid left-hand side expression in postfix operation
 
+//! Math
+
+// operand1 + operand2
+console.log(1 + 2); // 3
+// operand1 - operand2
+console.log(3 - 1); // 2
+// operand1 * operand2
+console.log(5 * 3); // 15
+// operand1 / operand2
+console.log(6 / 3); // 2
+// operand1 % operand2
+console.log(8 % 3); // 2
+console.log(9 % 4); // 1
+console.log(10 % 3); // 1
+// operand1 ** operand2
+console.log(8 ** 3); // 512 // 8 ni 3chi darajasi
+

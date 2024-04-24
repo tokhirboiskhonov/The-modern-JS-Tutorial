716578f4038c73be50774261555f2921b1dca4da
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-24 22:43:52 +0500

    add new function to understanding functions topic

--- js-demo.js
+++ js-demo.js
@@ -346,3 +346,14 @@ function checkScope() {
 
 let s = checkScope()();
 console.log(s);
+
+let calculator = {
+  operand1: 1,
+  operand2: 1,
+  add() {
+    this.result = this.operand1 + this.operand2;
+  },
+};
+calculator.add();
+
+console.log(calculator.result);
